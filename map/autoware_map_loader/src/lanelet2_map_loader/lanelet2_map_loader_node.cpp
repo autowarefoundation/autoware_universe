f26113a0f6c80739f355ab4d1005373588ba38ca
Ryohsuke Mitsudome <ryohsuke.mitsudome@tier4.jp>
2025-03-22 23:11:59 +0900

    fix: colcon build and test error
    Signed-off-by: Ryohsuke Mitsudome <ryohsuke.mitsudome@tier4.jp>

--- map/autoware_map_loader/src/lanelet2_map_loader/lanelet2_map_loader_node.cpp
+++ map/autoware_map_loader/src/lanelet2_map_loader/lanelet2_map_loader_node.cpp
@@ -51,9 +51,11 @@
 #include <lanelet2_projection/UTM.h>
 
 #include <filesystem>
+#include <map>
 #include <memory>
 #include <stdexcept>
 #include <string>
+#include <vector>
 
 namespace autoware::map_loader
 {
@@ -90,7 +92,7 @@ Lanelet2MapLoaderNode::Lanelet2MapLoaderNode(const rclcpp::NodeOptions & options
     [this](const MapProjectorInfo::Message::ConstSharedPtr msg) { on_map_projector_info(msg); });
 
   declare_parameter<bool>("allow_unsupported_version");
-  declare_parameter<std::vector<std::string>>(">lanelet2_map_paths_or_directory");
+  declare_parameter<std::string>("lanelet2_map_path");
   declare_parameter<double>("center_line_resolution");
   declare_parameter<bool>("use_waypoints");
   declare_parameter<bool>("enable_differential_map_loading");
@@ -105,7 +107,8 @@ void Lanelet2MapLoaderNode::on_map_projector_info(
   const MapProjectorInfo::Message::ConstSharedPtr msg)
 {
   const auto allow_unsupported_version = get_parameter("allow_unsupported_version").as_bool();
-  const auto lanelet2_paths_or_directory = get_parameter("lanelet2_map_path").as_string_array();
+  const auto lanelet2_paths_or_directory = std::vector<std::string>{
+    get_parameter("lanelet2_map_path").as_string()};
   const auto center_line_resolution = get_parameter("center_line_resolution").as_double();
   const auto use_waypoints = get_parameter("use_waypoints").as_bool();
   const auto enable_differential_map_loading =
