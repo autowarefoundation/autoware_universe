18845bb3b7b53d16e9b8b9c24c9542d0bd078f87
kyoichi-sugahara <kyoichi.sugahara@tier4.jp>
2025-04-01 17:38:47 +0900

    refactor(autoware_planning_validator): optimize trajectory analysis functions
    Signed-off-by: kyoichi-sugahara <kyoichi.sugahara@tier4.jp>

--- planning/autoware_planning_validator/include/autoware/planning_validator/utils.hpp
+++ planning/autoware_planning_validator/include/autoware/planning_validator/utils.hpp
@@ -15,18 +15,36 @@
 #ifndef AUTOWARE__PLANNING_VALIDATOR__UTILS_HPP_
 #define AUTOWARE__PLANNING_VALIDATOR__UTILS_HPP_
 
-#include <rclcpp/rclcpp.hpp>
+#include "autoware_vehicle_info_utils/vehicle_info_utils.hpp"
 
+#include <autoware_utils/geometry/boost_geometry.hpp>
+
+#include <autoware_perception_msgs/msg/predicted_objects.hpp>
 #include <autoware_planning_msgs/msg/trajectory.hpp>
 
-#include <string>
+#include <boost/geometry.hpp>
+#include <boost/geometry/geometries/box.hpp>
+#include <boost/geometry/geometries/point.hpp>
+#include <boost/geometry/index/rtree.hpp>
+
 #include <utility>
 #include <vector>
 
 namespace autoware::planning_validator
 {
+using autoware::vehicle_info_utils::VehicleInfo;
+using autoware_perception_msgs::msg::PredictedObject;
+using autoware_perception_msgs::msg::PredictedObjects;
+using autoware_perception_msgs::msg::PredictedPath;
+using autoware_perception_msgs::msg::Shape;
 using autoware_planning_msgs::msg::Trajectory;
 using autoware_planning_msgs::msg::TrajectoryPoint;
+using autoware_utils::Polygon2d;
+using Point = autoware_utils::Point2d;
+
+using Box = boost::geometry::model::box<Point>;
+using BoxTimeIndexPair = std::pair<Box, std::pair<double, std::size_t>>;
+using Rtree = boost::geometry::index::rtree<BoxTimeIndexPair, boost::geometry::index::rstar<16, 4>>;
 
 std::pair<double, size_t> getAbsMaxValAndIdx(const std::vector<double> & v);
 
@@ -41,9 +59,21 @@ void calcSteeringAngles(
 
 std::pair<double, size_t> calcMaxCurvature(const Trajectory & trajectory);
 
+void calc_interval_distance(
+  const Trajectory & trajectory, std::vector<double> & interval_distance_arr);
+
 std::pair<double, size_t> calcMaxIntervalDistance(const Trajectory & trajectory);
 
+void calc_lateral_acceleration(
+  const Trajectory & trajectory, std::vector<double> & lateral_acceleration_arr);
+
 std::pair<double, size_t> calcMaxLateralAcceleration(const Trajectory & trajectory);
+
+void calc_interval_time(const Trajectory & trajectory, std::vector<double> & time_interval_arr);
+
+void calc_lateral_jerk(const Trajectory & trajectory, std::vector<double> & lateral_jerk_arr);
+
+std::pair<double, size_t> calc_max_lateral_jerk(const Trajectory & trajectory);
 
 std::pair<double, size_t> getMaxLongitudinalAcc(const Trajectory & trajectory);
 
@@ -56,6 +86,31 @@ std::pair<double, size_t> calcMaxSteeringAngles(
 
 std::pair<double, size_t> calcMaxSteeringRates(
   const Trajectory & trajectory, const double wheelbase);
+
+std::optional<std::pair<std::vector<TrajectoryPoint>, std::vector<Box>>> check_collision(
+  const PredictedObjects & predicted_objects, const Trajectory & trajectory,
+  const geometry_msgs::msg::Point & current_ego_position, const VehicleInfo & vehicle_info,
+  const double trajectory_to_object_distance_threshold,
+  const double ego_to_object_distance_threshold, const double time_tolerance_threshold);
+
+Rtree make_ego_footprint_rtree(
+  std::vector<autoware_planning_msgs::msg::TrajectoryPoint> & trajectory,
+  const VehicleInfo & vehicle_info);
+
+std::optional<PredictedObjects> filter_objects(
+  const PredictedObjects & objects,
+  const std::vector<autoware_planning_msgs::msg::TrajectoryPoint> & trajectory,
+  const double trajectory_to_object_distance_threshold,
+  const double ego_to_object_distance_threshold);
+
+std::optional<PredictedPath> find_highest_confidence_path(const PredictedObject & object);
+
+void make_predicted_object_rtree(
+  const PredictedPath & highest_confidence_path, const Shape & object_shape,
+  const double predicted_time_step, std::vector<BoxTimeIndexPair> & predicted_object_rtree_nodes);
+
+std::vector<std::pair<size_t, Box>> detect_collisions(
+  const Rtree & ego_rtree, const Rtree & predicted_object_rtree, double time_tolerance);
 
 bool checkFinite(const TrajectoryPoint & point);
 
