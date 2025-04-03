f477fa1c2768e35ce35fde15839c32c40481ae3e
Maxime CLEMENT <maxime.clement@tier4.jp>
2025-04-03 11:51:03 +0900

    fix after move of MVP common and node packages to core
    Signed-off-by: Maxime CLEMENT <maxime.clement@tier4.jp>

--- planning/motion_velocity_planner/autoware_motion_velocity_run_out_module/src/run_out_module.hpp
+++ planning/motion_velocity_planner/autoware_motion_velocity_run_out_module/src/run_out_module.hpp
@@ -19,8 +19,8 @@
 #include "types.hpp"
 
 #include <autoware/motion_utils/marker/virtual_wall_marker_creator.hpp>
-#include <autoware/motion_velocity_planner_common_universe/plugin_module_interface.hpp>
-#include <autoware/motion_velocity_planner_common_universe/velocity_planning_result.hpp>
+#include <autoware/motion_velocity_planner_common/plugin_module_interface.hpp>
+#include <autoware/motion_velocity_planner_common/velocity_planning_result.hpp>
 #include <autoware/universe_utils/system/time_keeper.hpp>
 #include <diagnostic_updater/diagnostic_updater.hpp>
 #include <rclcpp/publisher.hpp>
