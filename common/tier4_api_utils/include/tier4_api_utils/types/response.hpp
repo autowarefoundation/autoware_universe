cce4e0768c08ffb0463fdbf4b8311ae5e2e2fe28
Takagi, Isamu <isamu.takagi@tier4.jp>
2025-04-22 18:28:23 +0900

    update detect function using C++17
    Signed-off-by: Takagi, Isamu <isamu.takagi@tier4.jp>

--- common/tier4_api_utils/include/tier4_api_utils/types/response.hpp
+++ common/tier4_api_utils/include/tier4_api_utils/types/response.hpp
@@ -48,7 +48,6 @@ inline bool is_error(const tier4_external_api_msgs::msg::ResponseStatus & status
 inline ResponseStatus response_success(const std::string & message = "")
 {
   return tier4_external_api_msgs::build<tier4_external_api_msgs::msg::ResponseStatus>()
-    .success(true)
     .code(tier4_external_api_msgs::msg::ResponseStatus::SUCCESS)
     .message(message);
 }
@@ -56,7 +55,6 @@ inline ResponseStatus response_success(const std::string & message = "")
 inline ResponseStatus response_ignored(const std::string & message = "")
 {
   return tier4_external_api_msgs::build<tier4_external_api_msgs::msg::ResponseStatus>()
-    .success(false)
     .code(tier4_external_api_msgs::msg::ResponseStatus::IGNORED)
     .message(message);
 }
@@ -64,7 +62,6 @@ inline ResponseStatus response_ignored(const std::string & message = "")
 inline ResponseStatus response_warn(const std::string & message = "")
 {
   return tier4_external_api_msgs::build<tier4_external_api_msgs::msg::ResponseStatus>()
-    .success(false)
     .code(tier4_external_api_msgs::msg::ResponseStatus::WARN)
     .message(message);
 }
@@ -72,7 +69,6 @@ inline ResponseStatus response_warn(const std::string & message = "")
 inline ResponseStatus response_error(const std::string & message = "")
 {
   return tier4_external_api_msgs::build<tier4_external_api_msgs::msg::ResponseStatus>()
-    .success(false)
     .code(tier4_external_api_msgs::msg::ResponseStatus::ERROR)
     .message(message);
 }
