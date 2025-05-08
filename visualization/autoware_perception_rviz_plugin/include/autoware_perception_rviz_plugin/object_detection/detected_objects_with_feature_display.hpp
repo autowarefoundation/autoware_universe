fa1e680ab138253831398c51c415dd3861ea298b
lei.gu <lei.gu@tier4.jp>
2025-05-08 17:56:16 +0900

    helper to better structure
    Signed-off-by: lei.gu <lei.gu@tier4.jp>

--- visualization/autoware_perception_rviz_plugin/include/autoware_perception_rviz_plugin/object_detection/detected_objects_with_feature_display.hpp
+++ visualization/autoware_perception_rviz_plugin/include/autoware_perception_rviz_plugin/object_detection/detected_objects_with_feature_display.hpp
@@ -15,6 +15,8 @@
 #define AUTOWARE_PERCEPTION_RVIZ_PLUGIN__OBJECT_DETECTION__DETECTED_OBJECTS_WITH_FEATURE_DISPLAY_HPP_
 
 #include "autoware_perception_rviz_plugin/object_detection/object_polygon_display_base.hpp"
+#include "detected_objects_with_feature_helper.hpp"
+
 
 #include <rviz_common/properties/bool_property.hpp>
 #include <rviz_common/properties/float_property.hpp>
@@ -35,116 +37,6 @@ namespace rviz_plugins
 {
 namespace object_detection
 {
-
-
-  class ColorbarWidget : public QWidget
-  {
-  public:
-  explicit ColorbarWidget(QWidget *parent = nullptr) : QWidget(parent) {
-        setFixedSize(100, 300);
-        setWindowTitle("Colorbar");
-        
-        m_font = QFont("Arial", 9);
-        m_font.setBold(true);
-
-        // Set default colors
-        m_textColor = Qt::white;
-        m_tickColor = Qt::white;
-        m_backgroundColor = QColor(0, 0, 0, 180);  // Semi-transparent dark background
-      }
-  
-      void setColorbarImage(const QImage &image) {
-          m_colorbar_image = image;
-          update();  // Trigger a repaint
-      }
-      void setMinMax(float min_value, float max_value)
-      {
-        m_min_value = min_value;
-        m_max_value = max_value;
-        update();
-      }
-      
-  protected:
-  void paintEvent(QPaintEvent *) override {
-    QPainter painter(this);
-    painter.setRenderHint(QPainter::Antialiasing);
-    
-    // Draw colorbar with margin
-    int colorbarWidth = 30;
-    int colorbarX = 10;
-    int colorbarY = 40;
-    int colorbarHeight = height() - 80;
-    
-    painter.drawImage(QRect(colorbarX, colorbarY, colorbarWidth, colorbarHeight), 
-                     m_colorbar_image.scaled(colorbarWidth, colorbarHeight));
-
-    // Draw frame around colorbar
-    painter.setPen(QPen(m_tickColor, 1.5));
-    painter.drawRect(colorbarX, colorbarY, colorbarWidth, colorbarHeight);
-
-    // Set font
-    painter.setFont(m_font);
-    painter.setPen(m_textColor);
-
-    // Draw title
-    painter.drawText(QRect(0, 10, width(), 20), Qt::AlignCenter, m_title);
-
-    // Draw ticks and labels
-    int numTicks = 5;
-    for (int i = 1; i < numTicks; ++i) {
-      float value = m_min_value + (m_max_value - m_min_value) * (i / float(numTicks));
-      int yPos = colorbarY + colorbarHeight - (i * colorbarHeight / numTicks);
-
-      // Draw tick
-      painter.setPen(QPen(m_tickColor, 2.0));
-      painter.drawLine(colorbarX + colorbarWidth, yPos, colorbarX + colorbarWidth + 5, yPos);
-      
-      // Draw value label
-      QString label = QString::number(value, 'f', 2);
-      QRect labelRect(
-        colorbarX + colorbarWidth + 10, yPos - 10, width() - colorbarX - colorbarWidth - 15, 20);
-
-      painter.setPen(m_textColor);
-      painter.drawText(labelRect, Qt::AlignLeft | Qt::AlignVCenter, label);
-    }
-    painter.setPen(QPen(m_tickColor, 2.5)); // Thicker for min/max
-    
-    // Min tick (bottom)
-    int minY = colorbarY + colorbarHeight;
-    painter.drawLine(colorbarX + colorbarWidth, minY, colorbarX + colorbarWidth + 10, minY);
-    
-    // Max tick (top)
-    int maxY = colorbarY;
-    painter.drawLine(colorbarX + colorbarWidth, maxY, colorbarX + colorbarWidth + 10, maxY);
-    // Draw min/max labels (more prominent)
-    painter.setPen(QPen(m_textColor, 1.5));
-    QFont boldFont = m_font;
-    boldFont.setPointSize(11);
-    painter.setFont(boldFont);
-    
-    // Min value
-    QRect minRect(colorbarX + colorbarWidth + 12, colorbarY + colorbarHeight - 15, 
-      width() - colorbarX - colorbarWidth - 20, 25);
-painter.drawText(minRect, Qt::AlignLeft | Qt::AlignVCenter, 
-         QString::number(m_min_value, 'f', 2));
-
-// Max value
-QRect maxRect(colorbarX + colorbarWidth + 12, colorbarY - 15, 
-      width() - colorbarX - colorbarWidth - 20, 25);
-painter.drawText(maxRect, Qt::AlignLeft | Qt::AlignVCenter, 
-         QString::number(m_max_value, 'f', 2));
-  }
-  
-  private:
-    QImage m_colorbar_image;
-    float m_min_value{0.0f};
-    float m_max_value{1.0f};
-    QString m_title;
-    QFont m_font;    
-    QColor m_textColor;
-    QColor m_tickColor;
-    QColor m_backgroundColor;
-  };
 
 
 /// \brief Class defining rviz plugin to visualize DetectedObjects
@@ -214,8 +106,7 @@ private:
   {
     m_marker_common.addMessage(marker_ptr);
   }
-    // Colorbar widget
-    ColorbarWidget* m_colorbar_widget;
+  ColorbarWidget* m_colorbar_widget;
 };
 
 }  // namespace object_detection
