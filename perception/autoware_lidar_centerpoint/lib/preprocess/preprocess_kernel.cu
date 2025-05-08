98d4aec5b9194017027530f67bffd957b1104790
kokseang <kokseang.tan@tier4.jp>
2025-05-08 15:51:14 +0900

    Add support of distance of z to pillar center in centerpoint

--- perception/autoware_lidar_centerpoint/lib/preprocess/preprocess_kernel.cu
+++ perception/autoware_lidar_centerpoint/lib/preprocess/preprocess_kernel.cu
@@ -39,7 +39,9 @@ namespace
 {
 const std::size_t MAX_POINT_IN_VOXEL_SIZE = 32;  // the same as max_point_in_voxel_size_ in config
 const std::size_t WARPS_PER_BLOCK = 4;
-const std::size_t ENCODER_IN_FEATURE_SIZE = 9;  // the same as encoder_in_feature_size_ in config
+// the same as num_pillars_per_block_ in config
+// x, y, z, time_lag, distance_x_to_cluster_center, distance_y_to_cluster_center, distance_z_to_cluster_center, distance_x_to_pillar_center, distance_y_to_pillar_center, distance_z_to_pillar_center
+const std::size_t ENCODER_IN_FEATURE_SIZE = 10;  // the same as encoder_in_feature_size_ in config
 }  // namespace
 
 namespace autoware::lidar_centerpoint
@@ -239,7 +241,7 @@ cudaError_t generateBaseFeatures_launch(
 __global__ void generateFeatures_kernel(
   const float * voxel_features, const float * voxel_num_points, const int * coords,
   const unsigned int * num_voxels, const float voxel_x, const float voxel_y, const float voxel_z,
-  const float range_min_x, const float range_min_y, const float range_min_z, float * features)
+  const float range_min_x, const float range_min_y, const float range_min_z, float * features, const std::size_t encoder_in_feature_size)
 {
   // voxel_features (float): (max_voxel_size, max_point_in_voxel_size, point_feature_size)
   // voxel_num_points (int): (max_voxel_size)
@@ -311,9 +313,11 @@ __global__ void generateFeatures_kernel(
 
     pillarOutSM[pillar_idx_inBlock][point_idx][7] = center.x;
     pillarOutSM[pillar_idx_inBlock][point_idx][8] = center.y;
+	pillarOutSM[pillar_idx_inBlock][point_idx][9] = center.z;
+  
+} else {
 
-  } else {
-    pillarOutSM[pillar_idx_inBlock][point_idx][0] = 0;
+	pillarOutSM[pillar_idx_inBlock][point_idx][0] = 0;
     pillarOutSM[pillar_idx_inBlock][point_idx][1] = 0;
     pillarOutSM[pillar_idx_inBlock][point_idx][2] = 0;
     pillarOutSM[pillar_idx_inBlock][point_idx][3] = 0;
@@ -324,14 +328,15 @@ __global__ void generateFeatures_kernel(
 
     pillarOutSM[pillar_idx_inBlock][point_idx][7] = 0;
     pillarOutSM[pillar_idx_inBlock][point_idx][8] = 0;
+    pillarOutSM[pillar_idx_inBlock][point_idx][9] = 0;
   }
 
   __syncthreads();
 
-  for (int i = 0; i < ENCODER_IN_FEATURE_SIZE; i++) {
+  for (int i = 0; i < encoder_in_feature_size; i++) {
     int outputSMId = pillar_idx_inBlock * MAX_POINT_IN_VOXEL_SIZE * ENCODER_IN_FEATURE_SIZE +
                      i * MAX_POINT_IN_VOXEL_SIZE + point_idx;
-    int outputId = pillar_idx * MAX_POINT_IN_VOXEL_SIZE * ENCODER_IN_FEATURE_SIZE +
+    int outputId = pillar_idx * MAX_POINT_IN_VOXEL_SIZE * encoder_in_feature_size +
                    i * MAX_POINT_IN_VOXEL_SIZE + point_idx;
     features[outputId] = ((float *)pillarOutSM)[outputSMId];
   }
@@ -342,13 +347,13 @@ cudaError_t generateFeatures_launch(
   const float * voxel_features, const float * voxel_num_points, const int * coords,
   const unsigned int * num_voxels, const std::size_t max_voxel_size, const float voxel_size_x,
   const float voxel_size_y, const float voxel_size_z, const float range_min_x,
-  const float range_min_y, const float range_min_z, float * features, cudaStream_t stream)
+  const float range_min_y, const float range_min_z, float * features, const std::size_t encoder_in_feature_size, cudaStream_t stream)
 {
   dim3 blocks(divup(max_voxel_size, WARPS_PER_BLOCK));
   dim3 threads(WARPS_PER_BLOCK * MAX_POINT_IN_VOXEL_SIZE);
   generateFeatures_kernel<<<blocks, threads, 0, stream>>>(
     voxel_features, voxel_num_points, coords, num_voxels, voxel_size_x, voxel_size_y, voxel_size_z,
-    range_min_x, range_min_y, range_min_z, features);
+    range_min_x, range_min_y, range_min_z, features, encoder_in_feature_size);
 
   return cudaGetLastError();
 }
