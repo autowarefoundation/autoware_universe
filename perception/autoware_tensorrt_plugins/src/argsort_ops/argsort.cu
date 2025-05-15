0668408eaa2d46a501070b562315a27896ac6ce0
Kenzo Lobos-Tsunekawa <kenokamikawa@gmail.com>
2025-05-15 15:51:52 +0900

    chore: replaced 0 by nullptr
    Signed-off-by: Kenzo Lobos-Tsunekawa <kenokamikawa@gmail.com>

--- perception/autoware_tensorrt_plugins/src/argsort_ops/argsort.cu
+++ perception/autoware_tensorrt_plugins/src/argsort_ops/argsort.cu
@@ -45,7 +45,7 @@ std::size_t get_argsort_workspace_size(std::size_t num_elements)
 
   cub::DeviceRadixSort::SortPairs(
     nullptr, temp_size, int64_nullptr, int64_nullptr, int64_nullptr, int64_nullptr, num_elements, 0,
-    64, 0);
+    64, nullptr);
 
   return temp_size;
 }
