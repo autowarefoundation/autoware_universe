1e75c857a404a61b30c28f97906685a301a9fd73
Samrat Thapa <samratthapa120@gmail.com>
2025-07-10 10:52:36 +0900

    added streampetr node
    Signed-off-by: Samrat Thapa <samratthapa120@gmail.com>

---
+++ perception/autoware_cameraonly_streampetr/include/autoware/camera_streampetr/network/network.hpp
@@ -0,0 +1,210 @@
+#ifndef AUTOWARE__CAMERA_STREAMPETR_NETWORK_HPP__
+#define AUTOWARE__CAMERA_STREAMPETR_NETWORK_HPP__
+
+
+#include <image_transport/image_transport.hpp>
+#include <memory>
+#include <rclcpp/rclcpp.hpp>
+#include <string>
+#include <map>
+
+#include <autoware_perception_msgs/msg/detected_objects.hpp>
+#include <nav_msgs/msg/odometry.hpp>
+#include <geometry_msgs/msg/pose.hpp>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
+#include <sensor_msgs/msg/image.hpp>
+#include <sensor_msgs/msg/camera_info.hpp>
+#include <tf2_ros/buffer.h>
+#include <tf2_ros/transform_listener.h>
+#include <tf2_ros/static_transform_broadcaster.h>
+#include <tf2_msgs/msg/tf_message.hpp>
+
+// From NVIDIA/DL4AGX
+#include <iostream>
+#include <fstream>
+#include <filesystem>
+#include <string>
+#include <vector>
+#include <memory>
+#include <chrono>
+#include <unordered_map>
+
+#include <cuda_fp16.h>
+#include <cuda_runtime_api.h>
+#include <NvInferRuntime.h>
+#include "autoware/camera_streampetr/network/memory.cuh"
+// From NVIDIA/DL4AGX
+
+#include "autoware/camera_streampetr/postprocess/non_maximum_supression.hpp"
+#include "autoware/camera_streampetr/postprocess/postprocess_kernel.hpp"
+#include "autoware/camera_streampetr/utils.hpp"
+#include "autoware/camera_streampetr/cuda_utils.hpp"
+
+namespace autoware::camera_streampetr
+{
+using namespace nvinfer1;
+using namespace cuda;
+
+class SubNetwork {
+  ICudaEngine* engine;
+  IExecutionContext* context; 
+public:
+  std::unordered_map<std::string, std::shared_ptr<Tensor>> bindings;
+  bool use_cuda_graph = false;
+  cudaGraph_t graph;
+  cudaGraphExec_t graph_exec;
+
+  SubNetwork(std::string engine_path, IRuntime* runtime) {
+    std::ifstream engine_file(engine_path, std::ios::binary);
+    if (!engine_file) {
+      throw std::runtime_error("Error opening engine file: " + engine_path);
+    }
+    engine_file.seekg(0, engine_file.end);
+    long int fsize = engine_file.tellg();
+    engine_file.seekg(0, engine_file.beg);
+
+    // Read the engine file into a buffer
+    std::vector<char> engineData(fsize);
+
+    engine_file.read(engineData.data(), fsize);
+    engine = runtime->deserializeCudaEngine(engineData.data(), fsize);
+    context = engine->createExecutionContext(); 
+
+    int nb = engine->getNbIOTensors();  
+
+    for( int n=0; n<nb; n++ ) {
+      std::string name = engine->getIOTensorName(n);
+      Dims d = engine->getTensorShape(name.c_str());            
+      DataType dtype = engine->getTensorDataType(name.c_str());
+      bindings[name] = std::make_shared<Tensor>(name, d, dtype);
+      bindings[name]->iomode = engine->getTensorIOMode(name.c_str());
+      std::cout << *(bindings[name]) << std::endl;
+      context->setTensorAddress(name.c_str(), bindings[name]->ptr);
+    }
+  }
+
+  void Enqueue(cudaStream_t stream) {
+    if( this->use_cuda_graph ) {
+      cudaGraphLaunch(graph_exec, stream);
+    } else {
+      context->enqueueV3(stream);
+    }  
+  }
+
+  ~SubNetwork() {
+  }
+
+  void EnableCudaGraph(cudaStream_t stream) {        
+    // run first time to avoid allocation
+    this->Enqueue(stream);
+    cudaStreamSynchronize(stream);
+
+    cudaStreamBeginCapture(stream, cudaStreamCaptureModeGlobal);
+    this->Enqueue(stream);
+    cudaStreamEndCapture(stream, &graph);
+    this->use_cuda_graph = true;
+#if CUDART_VERSION < 12000
+    cudaGraphInstantiate(&graph_exec, graph, NULL, NULL, 0);
+#else
+    cudaGraphInstantiate(&graph_exec, graph, 0);
+#endif
+  }
+}; // class SubNetwork
+
+
+class Duration {
+  // stat
+  std::vector<float> stats;
+  cudaEvent_t b, e;
+  std::string m_name;
+public:
+  Duration(std::string name): m_name(name) {
+    cudaEventCreate(&b);
+    cudaEventCreate(&e);
+  }
+
+  void MarkBegin(cudaStream_t s) {
+    cudaEventRecord(b, s);
+  }
+
+  void MarkEnd(cudaStream_t s) {
+    cudaEventRecord(e, s);
+  }
+
+  float Elapsed() {
+    float val;
+    cudaEventElapsedTime(&val, b, e);
+    stats.push_back(val);
+    return val;
+  }
+}; // class Duration
+
+class Logger : public ILogger {
+public:
+void log(Severity severity, const char* msg) noexcept override {
+  // Only print error messages
+  if (severity == Severity::kERROR) {
+    std::cerr << msg << std::endl;
+  }
+}
+};
+
+Logger gLogger;
+
+class StreamPetrNetwork {
+public:
+  StreamPetrNetwork(const std::string & engine_backbone_path, 
+  const std::string & engine_head_path, 
+  const std::string & engine_position_embedding_path, 
+  const bool use_temporal, 
+  const double search_distance_2d, 
+  const double circle_nms_dist_threshold,
+  const double iou_threshold, 
+  const double confidence_threshold,
+  const std::vector<std::string> class_names,
+  const int32_t num_proposals,
+  const std::vector<double> yaw_norm_thresholds,
+  const std::vector<float> detection_range);
+
+  ~StreamPetrNetwork();
+  void inference_detector(
+    const std::shared_ptr<Tensor> imgs,
+    const std::vector<float> & ego_pose,
+    const std::vector<float> & ego_pose_inv,
+    const std::vector<float> & img_metas_pad,
+    const std::vector<float> & intrinsics,
+    const std::vector<float> & img2lidar,
+    const float stamp,
+    std::vector<autoware_perception_msgs::msg::DetectedObject> & output_objects,
+    std::vector<float> & forward_time_ms);
+  
+  void printBindingInfo();
+
+private:
+  autoware_perception_msgs::msg::DetectedObject bbox_to_ros_msg(const Box3D & bbox);
+
+  std::unique_ptr<SubNetwork> backbone_;
+  std::unique_ptr<SubNetwork> pts_head_;
+  std::unique_ptr<SubNetwork> pos_embed_;
+
+  std::unique_ptr<Duration> dur_backbone_;
+  std::unique_ptr<Duration> dur_ptshead_;
+  std::unique_ptr<Duration> dur_pos_embed_;
+  std::unique_ptr<Duration> dur_postprocess_;
+
+
+  std::unique_ptr<PostprocessCuda> postprocess_cuda_;
+  NonMaximumSuppression iou_bev_nms_;
+  bool use_iou_bev_nms_ = false;
+
+  bool is_inference_initialized_ = false;
+  const bool use_temporal_;
+  Memory mem_;
+  cudaStream_t stream_;
+  const double confidence_threshold_;
+  const std::vector<std::string> class_names_;
+};
+
+}  // namespace autoware::camera_streampetr
+
+#endif  // AUTOWARE__CAMERA_STREAMPETR_NETWORK_HPP__
---
+++ perception/autoware_cameraonly_streampetr/include/autoware/camera_streampetr/node.hpp
@@ -0,0 +1,118 @@
+// Copyright 2025 TIER IV
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef AUTOWARE__CAMERA_STREAMPETR_NODE_HPP__
+#define AUTOWARE__CAMERA_STREAMPETR_NODE_HPP__
+
+
+#include <image_transport/image_transport.hpp>
+#include <memory>
+#include <rclcpp/rclcpp.hpp>
+#include <string>
+#include <map>
+
+#include <autoware_utils/ros/debug_publisher.hpp>
+#include <autoware_utils/ros/published_time_publisher.hpp>
+#include <autoware_utils/system/stop_watch.hpp>
+#include <autoware_perception_msgs/msg/detected_objects.hpp>
+
+
+#include <nav_msgs/msg/odometry.hpp>
+#include <geometry_msgs/msg/pose.hpp>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
+#include <sensor_msgs/msg/image.hpp>
+#include <sensor_msgs/msg/compressed_image.hpp>
+#include <sensor_msgs/msg/camera_info.hpp>
+#include <tf2_ros/buffer.h>
+#include <tf2_ros/transform_listener.h>
+#include <tf2_ros/static_transform_broadcaster.h>
+#include <tf2_msgs/msg/tf_message.hpp>
+
+// From NVIDIA/DL4AGX
+#include <iostream>
+#include <fstream>
+#include <filesystem>
+#include <string>
+#include <vector>
+#include <memory>
+#include <chrono>
+#include <unordered_map>
+
+#include <cuda_fp16.h>
+#include <cuda_runtime_api.h>
+#include <NvInferRuntime.h>
+// From NVIDIA/DL4AGX
+
+#include "autoware/camera_streampetr/network/camera_data_store.hpp"
+#include "autoware/camera_streampetr/network/network.hpp"
+
+namespace autoware::camera_streampetr
+{
+
+
+class StreamPetrNode : public rclcpp::Node
+{
+  using Odometry = nav_msgs::msg::Odometry;
+  using Image = sensor_msgs::msg::Image;
+  using CompressedImage = sensor_msgs::msg::CompressedImage;
+  using CameraInfo = sensor_msgs::msg::CameraInfo;
+  using DetectedObjects = autoware_perception_msgs::msg::DetectedObjects;
+  using DetectedObject = autoware_perception_msgs::msg::DetectedObject;
+
+public:
+  explicit StreamPetrNode(const rclcpp::NodeOptions & node_options);
+
+private:
+  void odometry_callback(Odometry::ConstSharedPtr input_msg);
+  void camera_info_callback(
+    CameraInfo::ConstSharedPtr input_camera_info_msg,
+    const int camera_id);
+  void camera_image_callback(
+    Image::ConstSharedPtr input_camera_image_msg,
+    const int camera_id);
+  void camera_image_callback(
+    CompressedImage::ConstSharedPtr input_camera_image_msg,
+    const int camera_id);
+  void step();
+  std::pair<std::vector<float>, std::vector<float>> get_ego_pose_vector() const;
+  std::vector<float> get_camera_extrinsics_vector(
+    const std::vector<std::string> & camera_links);
+  rclcpp::Subscription<Odometry>::SharedPtr localization_sub_;
+  std::vector<rclcpp::Subscription<CameraInfo>::SharedPtr> camera_info_subs_;
+  std::vector<rclcpp::Subscription<Image>::SharedPtr> camera_image_subs_;
+  std::vector<rclcpp::Subscription<CompressedImage>::SharedPtr> compressed_camera_image_subs_;
+  rclcpp::Publisher<DetectedObjects>::SharedPtr pub_objects_;
+
+  tf2_ros::Buffer tf_buffer_;
+  tf2_ros::TransformListener tf_listener_;
+  const size_t rois_number_;
+
+  Odometry::ConstSharedPtr initial_kinematic_state_ = nullptr;
+  Odometry::ConstSharedPtr latest_kinematic_state_ = nullptr;
+  std::unique_ptr<CameraDataStore> data_store_;
+  const bool is_compressed_image_;
+  const float max_camera_time_diff_;
+  const int anchor_camera_id_;
+  std::unique_ptr<StreamPetrNetwork> network_;
+  bool cycle_started_;
+
+  // debugger
+  std::unique_ptr<autoware_utils::StopWatch<std::chrono::milliseconds>> stop_watch_ptr_{nullptr};
+  std::unique_ptr<autoware_utils::DebugPublisher> debug_publisher_ptr_{nullptr};  
+  const bool debug_mode_;
+};
+
+}  // namespace autoware::camera_streampetr
+
+#endif  // AUTOWARE__CAMERA_STREAMPETR_NODE_HPP__
