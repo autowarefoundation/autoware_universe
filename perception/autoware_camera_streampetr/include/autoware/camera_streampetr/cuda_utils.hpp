168951c0b337d1bbdb732452363ea5517431017e
Samrat Thapa <samratthapa120@gmail.com>
2025-07-17 13:34:51 +0900

    use autoware_utils

--- perception/autoware_camera_streampetr/include/autoware/camera_streampetr/cuda_utils.hpp
+++ perception/autoware_camera_streampetr/include/autoware/camera_streampetr/cuda_utils.hpp
@@ -54,7 +54,9 @@
 #include <type_traits>
 #include <vector>
 
-#define CHECK_CUDA_ERROR(e) (cuda::check_error(e, __FILE__, __LINE__))
+#include <autoware/cuda_utils/cuda_check_error.hpp>
+#include <autoware/cuda_utils/cuda_unique_ptr.hpp>
+#include <autoware/cuda_utils/cuda_utils.hpp>
 
 namespace cuda
 {
@@ -150,7 +152,7 @@ struct Tensor
   }
 
   /**
-   * @brief Save tensor data as a numpy array file (.npy)
+   * @brief Save tensor data as a numpy array file (.npy). Useful for debugging.
    * @param filepath Path where to save the numpy file
    * @return true if successful, false otherwise
    */
@@ -297,73 +299,7 @@ inline std::ostream & operator<<(std::ostream & os, Tensor & t)
   return os;
 }
 
-inline void check_error(const ::cudaError_t e, const char * f, int n)
-{
-  if (e != ::cudaSuccess) {
-    ::std::stringstream s;
-    s << ::cudaGetErrorName(e) << " (" << e << ")@" << f << "#L" << n << ": "
-      << ::cudaGetErrorString(e);
-    throw ::std::runtime_error{s.str()};
-  }
-}
 
-struct deleter
-{
-  void operator()(void * p) const { CHECK_CUDA_ERROR(::cudaFree(p)); }
-};
-
-template <typename T>
-using unique_ptr = ::std::unique_ptr<T, deleter>;
-
-template <typename T>
-typename ::std::enable_if<::std::is_array<T>::value, cuda::unique_ptr<T>>::type make_unique(
-  const ::std::size_t n)
-{
-  using U = typename ::std::remove_extent<T>::type;
-  U * p;
-  CHECK_CUDA_ERROR(::cudaMalloc(reinterpret_cast<void **>(&p), sizeof(U) * n));
-  return cuda::unique_ptr<T>{p};
-}
-
-template <typename T>
-cuda::unique_ptr<T> make_unique()
-{
-  T * p;
-  CHECK_CUDA_ERROR(::cudaMalloc(reinterpret_cast<void **>(&p), sizeof(T)));
-  return cuda::unique_ptr<T>{p};
-}
-
-constexpr std::size_t CUDA_ALIGN = 256;
-
-template <typename T>
-inline std::size_t get_size_aligned(size_t num_elem)
-{
-  std::size_t size = num_elem * sizeof(T);
-  std::size_t extra_align = 0;
-  if (size % CUDA_ALIGN != 0) {
-    extra_align = CUDA_ALIGN - size % CUDA_ALIGN;
-  }
-  return size + extra_align;
-}
-
-template <typename T>
-inline T * get_next_ptr(size_t num_elem, void *& workspace, std::size_t & workspace_size)
-{
-  std::size_t size = get_size_aligned<T>(num_elem);
-  if (size > workspace_size) {
-    throw ::std::runtime_error("Workspace is too small!");
-  }
-  workspace_size -= size;
-  T * ptr = reinterpret_cast<T *>(workspace);
-  workspace = reinterpret_cast<void *>(reinterpret_cast<uintptr_t>(workspace) + size);
-  return ptr;
-}
-
-template <typename T>
-void clear_async(T * ptr, std::size_t num_elem, cudaStream_t stream)
-{
-  CHECK_CUDA_ERROR(::cudaMemsetAsync(ptr, 0, sizeof(T) * num_elem, stream));
-}
 
 }  // namespace cuda
 
