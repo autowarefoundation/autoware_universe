e38a68f6cc5ee0c4ba3ed6b35daca6a539f3e84f
Maxime CLEMENT <maxime.clement@tier4.jp>
2025-08-12 15:29:39 +0900

    split hpp/cpp
    Signed-off-by: Maxime CLEMENT <maxime.clement@tier4.jp>

--- common/autoware_boundary_departure_checker/include/autoware/boundary_departure_checker/steering_abnormality_utils.hpp
+++ common/autoware_boundary_departure_checker/include/autoware/boundary_departure_checker/steering_abnormality_utils.hpp
@@ -30,110 +30,33 @@
 
 namespace autoware::boundary_departure_checker::utils::steering
 {
-inline geometry_msgs::msg::Pose update_pose_with_bicycle_model(
+/**
+ * @brief simulate the motion of the given pose using a bicycle model
+ *
+ * @param pose initial pose
+ * @param steering_angle [rad] initial steering angle
+ * @param velocity [m/s] initial velocity
+ * @param dt [s] duration to simulate
+ * @param wheel_base [m] wheel base of the vehicle
+ */
+geometry_msgs::msg::Pose update_pose_with_bicycle_model(
   const geometry_msgs::msg::Pose & pose, const double steering_angle, const double velocity,
-  const double dt, const double wheel_base)
-{
-  const auto old_heading = tf2::getYaw(pose.orientation);
-  const double rotation_rate = velocity * std::tan(steering_angle) / wheel_base;
-
-  constexpr double epsilon = 1e-8;
-  auto updated_pose = pose;
-  if (std::abs(rotation_rate) < epsilon) {  // Case 1: straight line
-    const double distance_traveled = velocity * dt;
-    updated_pose.position.x += distance_traveled * std::cos(old_heading);
-    updated_pose.position.y += distance_traveled * std::sin(old_heading);
-  } else {  // Case 2: arc
-    const double turning_radius = velocity / rotation_rate;
-    const double new_heading = old_heading + rotation_rate * dt;
-    updated_pose.position.x += turning_radius * (std::sin(new_heading) - std::sin(old_heading));
-    updated_pose.position.y += turning_radius * (std::cos(old_heading) - std::cos(new_heading));
-    updated_pose.orientation = autoware_utils_geometry::create_quaternion_from_yaw(new_heading);
-  }
-  return updated_pose;
-}
+  const double dt, const double wheel_base);
 
 /**
- * @brief Generate vehicle footprints that reflect the vehicle's steering behavior.
- *
- * This function creates a wider footprint when the steering angle is high, simulating the way
- * a turning vehicle sweeps outward. The lateral margin increases over time based on an assumed
- * steering rate. The output is useful for predicting how the vehicle might occupy space during
- * turns.
+ * @brief Generate vehicle footprints along the given trajectory using some steering issues.
  *
  * @param trajectory        Predicted trajectory of the ego vehicle.
  * @param vehicle_info      Vehicle dimensions.
  * @param current_steering  Latest steering angle from the vehicle.
  * @return Footprints that adapt laterally to simulate steering influence over time.
  */
-inline std::vector<autoware_utils_geometry::LinearRing2d> create_vehicle_footprints(
+std::vector<autoware_utils_geometry::LinearRing2d> create_vehicle_footprints(
   const std::vector<autoware_planning_msgs::msg::TrajectoryPoint> & trajectory,
   const vehicle_info_utils::VehicleInfo & vehicle_info,
   [[maybe_unused]] const autoware_vehicle_msgs::msg::SteeringReport & current_steering,
-  const SteeringConfig & config)
-{
-  std::vector<autoware_utils_geometry::LinearRing2d> vehicle_footprints;
-  vehicle_footprints.reserve(trajectory.size());
-  const auto local_vehicle_footprint = vehicle_info.createFootprint(0.0, 0.0, 0.0, 0.0, 0.0, true);
-  if (trajectory.size() < 2) {
-    for (const auto & p : trajectory) {
-      vehicle_footprints.push_back(
-        transform_vector(local_vehicle_footprint, autoware_utils_geometry::pose2transform(p.pose)));
-      return vehicle_footprints;
-    }
-  }
+  const SteeringConfig & config);
 
-  vehicle_footprints.push_back(transform_vector(
-    local_vehicle_footprint, autoware_utils_geometry::pose2transform(trajectory.front().pose)));
-  std::vector<double> original_steering_changes;
-  original_steering_changes.reserve(trajectory.size());
-  auto t = 0.0;
-  std::optional<size_t>
-    delayed_index;  // the delay assumes a constant time interval along the trajectory
-  for (auto i = 0UL; i + 1 < trajectory.size(); ++i) {
-    original_steering_changes.push_back(
-      trajectory[i + 1].front_wheel_angle_rad - trajectory[i].front_wheel_angle_rad);
-    if (t >= config.delay_s) {
-      if (!delayed_index) {
-        delayed_index = i;
-      }
-    } else {
-      vehicle_footprints.push_back(transform_vector(
-        local_vehicle_footprint, autoware_utils_geometry::pose2transform(trajectory[i].pose)));
-      const auto dt = rclcpp::Duration(trajectory[i + 1].time_from_start) -
-                      rclcpp::Duration(trajectory[i].time_from_start);
-      t += dt.seconds();
-    }
-  }
-  if (!delayed_index) {
-    delayed_index = 0UL;
-  }
-  auto pose = trajectory[*delayed_index].pose;
-  auto steering_angle = static_cast<double>(trajectory[*delayed_index].front_wheel_angle_rad);
-  for (auto i = *delayed_index; i + 1 < trajectory.size(); ++i) {
-    const auto prev_p = trajectory[i];
-    const auto curr_p = trajectory[i + 1];
-    const auto dt =
-      rclcpp::Duration(curr_p.time_from_start) - rclcpp::Duration(prev_p.time_from_start);
-    const auto v = (prev_p.longitudinal_velocity_mps + curr_p.longitudinal_velocity_mps) * 0.5;
-    // simulate the ego vehicle motion
-    pose = update_pose_with_bicycle_model(
-      pose, steering_angle, v, dt.seconds(), vehicle_info.wheel_base_m);
-    vehicle_footprints.push_back(
-      transform_vector(local_vehicle_footprint, autoware_utils::pose2transform(pose)));
-
-    // update the simulated steering angle
-    const auto steering_change = original_steering_changes[i - *delayed_index] * config.factor +
-                                 config.offset_rps * dt.seconds();
-    const auto steering_rate_limit = autoware::interpolation::lerp(
-      config.steering_rate_velocities_mps, config.steering_rate_limits_rps, v);
-    const auto max_steering_change = steering_rate_limit * dt.seconds();
-    steering_angle += std::clamp(steering_change, -max_steering_change, max_steering_change);
-    steering_angle = std::clamp(
-      steering_angle, -vehicle_info.max_steer_angle_rad, vehicle_info.max_steer_angle_rad);
-  }
-  return vehicle_footprints;
-}
 }  // namespace autoware::boundary_departure_checker::utils::steering
 
 #endif  // AUTOWARE__BOUNDARY_DEPARTURE_CHECKER__STEERING_ABNORMALITY_UTILS_HPP_
