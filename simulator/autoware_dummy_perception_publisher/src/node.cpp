df8c928e7ec2401214359de7699565cc16c2d3a1
Daniel Sanchez <danielsanchezaran@gmail.com>
2025-08-25 16:56:27 +0900

    fix style issues
    Signed-off-by: Daniel Sanchez <danielsanchezaran@gmail.com>

--- simulator/autoware_dummy_perception_publisher/src/node.cpp
+++ simulator/autoware_dummy_perception_publisher/src/node.cpp
@@ -77,12 +77,6 @@ ObjectInfo::ObjectInfo(
   const double elapsed_time = current_time.seconds() - rclcpp::Time(object.header.stamp).seconds();
   double current_vel = initial_vel + initial_acc * elapsed_time;
   if (initial_acc != 0.0) {
-    if (initial_acc < 0 && 0 < initial_vel) {
-      current_vel = std::max(current_vel, 0.0);
-    }
-    if (0 < initial_acc && initial_vel < 0) {
-      current_vel = std::min(current_vel, 0.0);
-    }
     current_vel = std::clamp(
       current_vel, static_cast<double>(object.min_velocity),
       static_cast<double>(object.max_velocity));
@@ -120,8 +114,7 @@ ObjectInfo::ObjectInfo(
     tf2::fromMsg(current_pose, tf_map2moved_object);
 
     // Set velocity from initial state
-    const double current_vel = object.initial_state.twist_covariance.twist.linear.x;
-    twist_covariance_.twist.linear.x = current_vel;
+    twist_covariance_.twist.linear.x = object.initial_state.twist_covariance.twist.linear.x;
     return;
   }
 
@@ -500,13 +493,13 @@ void DummyPerceptionPublisherNode::timerCallback()
 
     ObjectInfo obj_info = [&]() {
       // Only use predicted motion if the action is PREDICT
-      if (object.action == tier4_simulation_msgs::msg::DummyObject::PREDICT && matched_predicted) {
-        return ObjectInfo(
-          object, predicted_object, predicted_time, current_time, predicted_path_delay_);
-      }
-
       // Check if we have a last used prediction for this object (only if action is PREDICT)
       if (object.action == tier4_simulation_msgs::msg::DummyObject::PREDICT) {
+        if (matched_predicted) {
+          return ObjectInfo(
+            object, predicted_object, predicted_time, current_time, predicted_path_delay_);
+        }
+
         const auto & dummy_uuid_str = autoware_utils_uuid::to_hex_string(object.id);
         auto last_used_pred_it = dummy_last_used_predictions_.find(dummy_uuid_str);
         auto last_used_time_it = dummy_last_used_prediction_times_.find(dummy_uuid_str);
