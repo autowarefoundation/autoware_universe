51f01f997fd2709df0ebadd1ac0348db57dfe3b3
Go Sakayori <go.sakayori@tier4.jp>
2025-09-05 12:34:23 +0900

    add trajectory adaptor
    Signed-off-by: Go Sakayori <go.sakayori@tier4.jp>

---
+++ planning/autoware_trajectory_adaptor/src/trajectory_adaptor.cpp
@@ -0,0 +1,79 @@
+// Copyright 2025 TIER IV, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "trajectory_adaptor.hpp"
+
+#include <autoware/motion_utils/trajectory/trajectory.hpp>
+
+#include <memory>
+
+namespace autoware::trajectory_adaptor
+{
+
+TrajectoryAdaptorNode::TrajectoryAdaptorNode(const rclcpp::NodeOptions & node_options)
+: Node{"trajectory_adaptor_node", node_options},
+  sub_trajectories_{this->create_subscription<ScoredCandidateTrajectories>(
+    "~/input/trajectories", 1,
+    std::bind(&TrajectoryAdaptorNode::process, this, std::placeholders::_1))},
+  pub_trajectory_{this->create_publisher<Trajectory>("~/output/trajectory", 1)}
+{
+  debug_processing_time_detail_pub_ = create_publisher<autoware_utils_debug::ProcessingTimeDetail>(
+    "~/debug/processing_time_detail_ms/trajectory_adaptor", 1);
+  time_keeper_ =
+    std::make_shared<autoware_utils_debug::TimeKeeper>(debug_processing_time_detail_pub_);
+}
+
+void TrajectoryAdaptorNode::process(const ScoredCandidateTrajectories::ConstSharedPtr msg)
+{
+  autoware_utils_debug::ScopedTimeTrack st(__func__, *time_keeper_);
+
+  if (msg->scored_candidate_trajectories.empty()) {
+    return;
+  }
+
+  const auto trajectory_itr = std::max_element(
+    msg->scored_candidate_trajectories.begin(), msg->scored_candidate_trajectories.end(),
+    [](const auto & a, const auto & b) { return a.score < b.score; });
+  if (trajectory_itr == msg->scored_candidate_trajectories.end()) {
+    return;
+  }
+
+  const auto best_generator = [&msg](const auto & uuid) {
+    const auto generator_itr = std::find_if(
+      msg->generator_info.begin(), msg->generator_info.end(),
+      [&uuid](const auto & info) { return info.generator_id == uuid; });
+    return generator_itr == msg->generator_info.end() ? "NOT FOUND"
+                                                      : generator_itr->generator_name.data;
+  };
+
+  RCLCPP_DEBUG_STREAM(
+    this->get_logger(),
+    "best generator:" << best_generator(trajectory_itr->candidate_trajectory.generator_id)
+                      << " score:" << trajectory_itr->score);
+
+  const auto traj_points = autoware::motion_utils::removeOverlapPoints(
+    trajectory_itr->candidate_trajectory
+      .points);  // to-do (go-sakayori): should be done in trajectory generator? Add more functions
+                 // to adjust the trajectory to autoware controller
+  const auto trajectory = autoware_planning_msgs::build<Trajectory>()
+                            .header(trajectory_itr->candidate_trajectory.header)
+                            .points(traj_points);
+
+  pub_trajectory_->publish(trajectory);
+}
+
+}  // namespace autoware::trajectory_adaptor
+
+#include <rclcpp_components/register_node_macro.hpp>
+RCLCPP_COMPONENTS_REGISTER_NODE(autoware::trajectory_adaptor::TrajectoryAdaptorNode)
