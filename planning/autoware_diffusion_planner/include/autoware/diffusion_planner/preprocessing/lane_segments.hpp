fecf54f6e0042efc4f2f67f6aab236e234eda0c0
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2025-09-08 14:42:41 +0900

    Fixed traffic_light_id
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>

--- planning/autoware_diffusion_planner/include/autoware/diffusion_planner/preprocessing/lane_segments.hpp
+++ planning/autoware_diffusion_planner/include/autoware/diffusion_planner/preprocessing/lane_segments.hpp
@@ -166,7 +166,6 @@ private:
   std::vector<autoware::diffusion_planner::LaneSegment> lane_segments_;
   Eigen::MatrixXd map_lane_segments_matrix_;
   ColLaneIDMaps col_id_mapping_;
-  const std::shared_ptr<lanelet::LaneletMap> lanelet_map_ptr_;
 };
 
 }  // namespace autoware::diffusion_planner::preprocess
