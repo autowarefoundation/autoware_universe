3f8f95b3b7ba4287e11de1126fde85ee9b7c4a16
yuki-takagi-66 <yuki.takagi@tier4.jp>
2025-09-11 16:21:20 +0900

    po
    Signed-off-by: yuki-takagi-66 <yuki.takagi@tier4.jp>

--- planning/motion_velocity_planner/autoware_motion_velocity_obstacle_slow_down_module/src/obstacle_slow_down_module.cpp
+++ planning/motion_velocity_planner/autoware_motion_velocity_obstacle_slow_down_module/src/obstacle_slow_down_module.cpp
@@ -729,10 +729,6 @@ std::vector<SlowdownInterval> ObstacleSlowDownModule::plan_slow_down(
     const auto slow_down_end_idx = dist_to_slow_down_start < dist_to_slow_down_end
                                      ? insert_point_in_trajectory(dist_to_slow_down_end)
                                      : std::nullopt;
-    if (!slow_down_end_idx) {
-      continue;
-    }
-
     // calculate slow down velocity
     const double stable_slow_down_vel = [&]() {
       if (prev_output) {
@@ -742,6 +738,22 @@ std::vector<SlowdownInterval> ObstacleSlowDownModule::plan_slow_down(
       }
       return feasible_slow_down_vel;
     }();
+
+    if (!slow_down_end_idx) {
+      continue;
+    }
+
+    if (std::all_of(
+          slow_down_traj_points.begin() + (slow_down_start_idx ? *slow_down_start_idx : 0),
+          slow_down_traj_points.begin() + *slow_down_end_idx,
+          [&](const auto & tp) { return stable_slow_down_vel >= tp.longitudinal_velocity_mps; })) {
+      RCLCPP_DEBUG(
+        logger_,
+        "[SlowDown] Ignore obstacle (%s) since slow down velocity (%f) is higher than trajectory "
+        "velocity.",
+        autoware_utils_uuid::to_hex_string(obstacle.uuid).c_str(), stable_slow_down_vel);
+      continue;
+    }
 
     // insert slow down velocity between slow start and end
     slowdown_intervals.push_back(
