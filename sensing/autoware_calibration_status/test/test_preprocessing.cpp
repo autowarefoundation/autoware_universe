074cd96798fbdf383e17b073252b54bf528b2db1
Amadeusz Szymko <amadeusz.szymko.2@tier4.jp>
2025-09-18 17:35:54 +0900

    test(autoware_calibration_status): make that CI skip unit tests
    Signed-off-by: Amadeusz Szymko <amadeusz.szymko.2@tier4.jp>

--- sensing/autoware_calibration_status/test/test_preprocessing.cpp
+++ sensing/autoware_calibration_status/test/test_preprocessing.cpp
@@ -68,6 +68,14 @@ protected:
                "data";
 
     samples.push_back(data_utils::load_test_sample(data_dir, "sample_102"));
+
+    const char * home_env = std::getenv("HOME");
+    std::filesystem::path home_path = home_env ? home_env : "/";
+    std::filesystem::path onnx_path =
+      home_path / "autoware_data/calibration_status/calibration_status.onnx";
+    if (!std::filesystem::exists(onnx_path)) {
+      GTEST_SKIP() << "ONNX model file not found: " << onnx_path;
+    }
   }
   static void TearDownTestSuite() {}
 };
