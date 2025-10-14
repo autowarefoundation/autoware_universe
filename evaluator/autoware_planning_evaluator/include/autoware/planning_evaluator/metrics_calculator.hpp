63068ac5fc1e7da75ca6ee7a64d13be54840cb91
xtk8532704 <1041084556@qq.com>
2025-10-14 18:18:04 +0900

    tmp save
    Signed-off-by: xtk8532704 <1041084556@qq.com>

--- evaluator/autoware_planning_evaluator/include/autoware/planning_evaluator/metrics_calculator.hpp
+++ evaluator/autoware_planning_evaluator/include/autoware/planning_evaluator/metrics_calculator.hpp
@@ -57,7 +57,9 @@ public:
 
     struct
     {
-      double dist_thr_m = 1.0;
+      double dist_thr_m = 0.0;
+      double limit_max_accel = 0.0; // TODO change to default value
+      double limit_min_accel = 0.0; // TODO change to default value
     } obstacle;
   } parameters;  // struct Parameters for those metrics calculated by the MetricsCalculator
 
