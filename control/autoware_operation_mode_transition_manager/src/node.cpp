16e26cb6ffd93023ce1767e3bd3b592b3c45c7d3
issei.tezuka <issei.tezuka@tier4.jp>
2025-11-06 18:05:44 +0900

    fix(operation_mode_transition_manager): modify the autonomous mode condition

--- control/autoware_operation_mode_transition_manager/src/node.cpp
+++ control/autoware_operation_mode_transition_manager/src/node.cpp
@@ -120,7 +120,7 @@ void OperationModeTransitionManager::changeOperationMode(std::optional<Operation
 {
   // NOTE: If request_mode is nullopt, indicates to enable autoware control
 
-  const bool current_control = control_mode_report_.mode == ControlModeReport::AUTONOMOUS;
+  const bool current_control = (control_mode_report_.mode == ControlModeReport::AUTONOMOUS || control_mode_report_.mode == ControlModeReport::AUTONOMOUS_VELOCITY_ONLY || control_mode_report_.mode == ControlModeReport::AUTONOMOUS_STEER_ONLY );
   const bool request_control = request_mode ? false : true;
 
   if (current_mode_ == request_mode) {
@@ -170,7 +170,7 @@ void OperationModeTransitionManager::cancelTransition()
 
 void OperationModeTransitionManager::processTransition(const InputData & input_data)
 {
-  const bool current_control = control_mode_report_.mode == ControlModeReport::AUTONOMOUS;
+  const bool current_control = (control_mode_report_.mode == ControlModeReport::AUTONOMOUS || control_mode_report_.mode == ControlModeReport::AUTONOMOUS_VELOCITY_ONLY || control_mode_report_.mode == ControlModeReport::AUTONOMOUS_STEER_ONLY );
 
   // Check timeout.
   if (transition_timeout_ < (now() - transition_->time).seconds()) {
@@ -316,7 +316,7 @@ InputData OperationModeTransitionManager::subscribeData()
 
 void OperationModeTransitionManager::publishData()
 {
-  const bool current_control = control_mode_report_.mode == ControlModeReport::AUTONOMOUS;
+  const bool current_control = (control_mode_report_.mode == ControlModeReport::AUTONOMOUS || control_mode_report_.mode == ControlModeReport::AUTONOMOUS_VELOCITY_ONLY || control_mode_report_.mode == ControlModeReport::AUTONOMOUS_STEER_ONLY );
   const auto time = now();
 
   OperationModeStateAPI::Message state;
