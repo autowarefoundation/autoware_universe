607dd3e509f8fe7d7625fb965b2927b812bca089
YuxuanLiuTier4Desktop <619684051@qq.com>
2025-11-13 11:48:17 +0900

    fix: clean up excessive logs and fix cig
    Signed-off-by: YuxuanLiuTier4Desktop <619684051@qq.com>

--- vehicle/autoware_generic_value_converter/src/value_map.cpp
+++ vehicle/autoware_generic_value_converter/src/value_map.cpp
@@ -56,52 +56,20 @@ bool ValueMap::getValue(const double acc, double vel, double & value) const
     return false;
   }
 
-  RCLCPP_DEBUG_THROTTLE(logger_, clock_, 5000, "getValue input: acc=%.3f, vel=%.3f", acc, vel);
-
   std::vector<double> interpolated_acc_vec;
   const double clamped_vel = CSVLoader::clampValue(vel, vel_index_, "value: vel");
-
-  RCLCPP_DEBUG_THROTTLE(
-    logger_, clock_, 5000, "Clamped velocity: %.3f (original: %.3f)", clamped_vel, vel);
 
   // (value, vel, acc) map => (value, acc) map by fixing vel
   interpolated_acc_vec.reserve(value_map_.size());
   for (const std::vector<double> & accelerations : value_map_) {
-    // Log the inputs to the first lerp function
-    if (interpolated_acc_vec.empty()) {
-      RCLCPP_DEBUG_THROTTLE(
-        logger_, clock_, 5000,
-        "First lerp call - vel_index size: %zu, accelerations size: %zu, clamped_vel: %.3f",
-        vel_index_.size(), accelerations.size(), clamped_vel);
-
-      // Log some sample values from the vectors
-      std::string vel_samples = "vel_index samples: ";
-      std::string acc_samples = "accelerations samples: ";
-      for (size_t i = 0; i < std::min(size_t(3), vel_index_.size()); ++i) {
-        vel_samples += std::to_string(vel_index_[i]) + " ";
-      }
-      for (size_t i = 0; i < std::min(size_t(3), accelerations.size()); ++i) {
-        acc_samples += std::to_string(accelerations[i]) + " ";
-      }
-      RCLCPP_DEBUG_THROTTLE(logger_, clock_, 5000, "%s", vel_samples.c_str());
-      RCLCPP_DEBUG_THROTTLE(logger_, clock_, 5000, "%s", acc_samples.c_str());
-    }
-
     interpolated_acc_vec.push_back(
       autoware::interpolation::lerp(vel_index_, accelerations, clamped_vel));
   }
 
-  RCLCPP_DEBUG_THROTTLE(
-    logger_, clock_, 5000, "Interpolated acceleration vector size: %zu",
-    interpolated_acc_vec.size());
-
-  // Log some sample values from the interpolated vector
-  if (!interpolated_acc_vec.empty()) {
-    std::string interp_samples = "interpolated_acc_vec samples: ";
-    for (size_t i = 0; i < std::min(size_t(3), interpolated_acc_vec.size()); ++i) {
-      interp_samples += std::to_string(interpolated_acc_vec[i]) + " ";
-    }
-    RCLCPP_DEBUG_THROTTLE(logger_, clock_, 5000, "%s", interp_samples.c_str());
+  // Make sure we have data to work with
+  if (interpolated_acc_vec.empty()) {
+    RCLCPP_WARN_THROTTLE(logger_, clock_, 5000, "Interpolated acceleration vector is empty");
+    return false;
   }
 
   // calculate value from acceleration
@@ -122,30 +90,7 @@ bool ValueMap::getValue(const double acc, double vel, double & value) const
     return true;
   }
 
-  // Log the inputs to the second lerp function
-  RCLCPP_DEBUG_THROTTLE(
-    logger_, clock_, 5000,
-    "Second lerp call - interpolated_acc_vec size: %zu, value_index size: %zu, acc: %.3f",
-    interpolated_acc_vec.size(), value_index_.size(), acc);
-
-  // Log some sample values from the vectors
-  std::string value_samples = "value_index_ samples: ";
-  std::string interpolated_acc_vec_samples = "interpolated_acc_vec samples: ";
-  for (size_t i = 0; i < value_index_.size(); ++i) {
-    value_samples += std::to_string(value_index_[i]) + " ";
-  }
-  for (size_t i = 0; i < interpolated_acc_vec.size(); ++i) {
-    interpolated_acc_vec_samples += std::to_string(interpolated_acc_vec[i]) + " ";
-  }
-  RCLCPP_DEBUG_THROTTLE(logger_, clock_, 5000, "%s", value_samples.c_str());
-  RCLCPP_DEBUG_THROTTLE(logger_, clock_, 5000, "%s", interpolated_acc_vec_samples.c_str());
-
   value = autoware::interpolation::lerp(interpolated_acc_vec, value_index_, acc);
-
-  RCLCPP_DEBUG_THROTTLE(
-    logger_, clock_, 5000, "Final interpolated value: %.3f for acc=%.3f, vel=%.3f", value, acc,
-    vel);
-
   return true;
 }
 
