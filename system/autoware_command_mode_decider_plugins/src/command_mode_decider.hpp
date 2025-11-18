ce9ee70af04c8e0aaf69d0795ffb1b988da3c127
Takagi, Isamu <isamu.takagi@tier4.jp>
2025-11-18 13:53:57 +0900

    fix(command_mode_decider): fix driving mode change condition
    Signed-off-by: Takagi, Isamu <isamu.takagi@tier4.jp>

--- system/autoware_command_mode_decider_plugins/src/command_mode_decider.hpp
+++ system/autoware_command_mode_decider_plugins/src/command_mode_decider.hpp
@@ -37,9 +37,11 @@ public:
 private:
   std::vector<uint16_t> decide(
     const RequestModeStatus & request, const CommandModeStatusTable & table,
-    const std::unordered_set<uint16_t> & last_modes) const;
+    const std::unordered_set<uint16_t> & last_modes,
+    const std::optional<bool> & last_autoware_control) const;
 
   std::unordered_set<uint16_t> last_modes_;
+  std::optional<bool> last_autoware_control_;
 };
 
 }  // namespace autoware::command_mode_decider
