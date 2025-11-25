0f7eb5dabb7bac7484908ae97859b5eafbbc509e
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2025-11-25 17:22:34 +0900

    Applied cpplint
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>

--- planning/autoware_diffusion_planner/test/lanelet_test.cpp
+++ planning/autoware_diffusion_planner/test/lanelet_test.cpp
@@ -23,8 +23,9 @@ TEST_F(LaneletTest, ConvertToLaneSegments)
 {
   auto lane_segments = convert_to_internal_lanelet_map(lanelet_map_);
 
-  EXPECT_EQ(lane_segments.lane_segments.size(), 1);                 // Expect one lanelet to be converted
-  EXPECT_EQ(lane_segments.lane_segments[0].centerline.size(), 10);  // Expect 10 points in the polyline
+  EXPECT_EQ(lane_segments.lane_segments.size(), 1);  // Expect one lanelet to be converted
+  EXPECT_EQ(
+    lane_segments.lane_segments[0].centerline.size(), 10);  // Expect 10 points in the polyline
 }
 
 }  // namespace autoware::diffusion_planner::test
