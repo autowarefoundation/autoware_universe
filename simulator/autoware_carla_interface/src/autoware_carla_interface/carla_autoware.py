60679e20ae44fd2c95cbc3d39c2728d358e3b34e
Max-Bin <vborisw@gmail.com>
2025-11-28 18:00:38 +0900

    feat(autoware_carla_interface): add E2E planning support and VAD integration
    Add comprehensive end-to-end (E2E) planning infrastructure to enable
    neural planners like VAD to directly control vehicles in CARLA simulation.
    
    Key features:
    - E2E planning components (velocity converter, state publisher, TF, controls)
    - CARLA utility C++ nodes (state publisher, operation mode publisher)
    - Multi-camera combiner for temporal synchronization
    - Conditional E2E activation via use_e2e_planning flag
    - Automatic CARLA map name derivation from map_path
    - Mixed Python/C++ package architecture
    
    Related to autoware_tensorrt_vad integration for end-to-end autonomous driving.
    
    Signed-off-by: Max-Bin <vborisw@gmail.com>

--- simulator/autoware_carla_interface/src/autoware_carla_interface/carla_autoware.py
+++ simulator/autoware_carla_interface/src/autoware_carla_interface/carla_autoware.py
@@ -28,6 +28,7 @@ from .modules.carla_wrapper import SensorWrapper
 
 
 class SensorLoop(object):
+
     def __init__(self):
         self.start_game_time = None
         self.start_system_time = None
@@ -55,6 +56,7 @@ class SensorLoop(object):
 
 
 class InitializeInterface(object):
+
     def __init__(self):
         self.interface = carla_ros2_interface()
         self.param_ = self.interface.get_param()
@@ -109,9 +111,9 @@ class InitializeInterface(object):
 
         self.sensor_wrapper = SensorWrapper(self.interface)
         self.sensor_wrapper.setup_sensors(self.ego_actor, False)
-        ##########################################################################################################################################################
+        ##########################################################################
         # TRAFFIC MANAGER
-        ##########################################################################################################################################################
+        ##########################################################################
         # cspell:ignore trafficmanager
         if self.use_traffic_manager:
             traffic_manager = client.get_trafficmanager()
@@ -174,9 +176,11 @@ class InitializeInterface(object):
         self.bridge_loop._stop_loop()
 
     def _cleanup(self):
-        """Clean up all CARLA resources in reverse initialization order.
+        """
+        Clean up all CARLA resources in reverse initialization order.
 
         Ensures cleanup happens even if individual steps fail.
+
         """
         self._cleanup_sensors()
         self._cleanup_ros_interface()
