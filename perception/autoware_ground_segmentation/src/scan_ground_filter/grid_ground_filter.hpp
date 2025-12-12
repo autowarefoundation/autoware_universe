d00c6964248b0206a0a0e0dcec703262c85f296a
badai-nguyen <dai.nguyen@tier4.jp>
2025-12-12 11:12:33 +0900

    fix: spelling check
    Signed-off-by: badai-nguyen <dai.nguyen@tier4.jp>

--- perception/autoware_ground_segmentation/src/scan_ground_filter/grid_ground_filter.hpp
+++ perception/autoware_ground_segmentation/src/scan_ground_filter/grid_ground_filter.hpp
@@ -164,7 +164,7 @@ public:
     grid_ptr_ = std::make_unique<Grid>(
       param_.virtual_lidar_x, param_.virtual_lidar_y, param_.virtual_lidar_z);
     // TODO(badai-nguyen): Temporary add radial limit to 200.0m constant value.
-    // need to be updated unify with crob range parameter
+    // need to be updated unify with cropbox range parameter
     grid_ptr_->initialize(param_.grid_size_m, param_.radial_divider_angle_rad, 200.0f);
   }
   ~GridGroundFilter() = default;
