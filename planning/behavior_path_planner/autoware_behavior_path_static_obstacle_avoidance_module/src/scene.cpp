071d63b6fab4ce2094c828a1fffb49f1dcf713c9
Mehmet Dogru <42mehmetdogru42@gmail.com>
2025-12-15 15:48:21 +0200

    feat(avoidance): enable signaling while avoidance yield
    Signed-off-by: Mehmet Dogru <42mehmetdogru42@gmail.com>

--- planning/behavior_path_planner/autoware_behavior_path_static_obstacle_avoidance_module/src/scene.cpp
+++ planning/behavior_path_planner/autoware_behavior_path_static_obstacle_avoidance_module/src/scene.cpp
@@ -1038,12 +1038,27 @@ auto StaticObstacleAvoidanceModule::getTurnSignal(
   };
 
   auto shift_lines = path_shifter_.getShiftLines();
+  auto selected_spline_shift_path = spline_shift_path;
+  auto selected_linear_shift_path = linear_shift_path;
+  if (avoid_data_.yield_required) {
+    if (avoid_data_.candidate_path.path.points.empty()) {
+      return getPreviousModuleOutput().turn_signal_info;
+    }
+
+    selected_spline_shift_path = avoid_data_.candidate_path;
+    selected_linear_shift_path = avoid_data_.candidate_path;
+
+    shift_lines.clear();
+    for (const auto & al : avoid_data_.safe_shift_line) {
+      shift_lines.push_back(al);
+    }
+  }
 
   const auto & ref_points = path_shifter_.getReferencePath().points;
   if (ref_points.empty()) {
     return getPreviousModuleOutput().turn_signal_info;
   }
-  
+
   const size_t ego_idx = planner_data_->findEgoIndex(ref_points);
 
   std::vector<ShiftLine> shift_lines_after_ego;
@@ -1057,7 +1072,7 @@ auto StaticObstacleAvoidanceModule::getTurnSignal(
     return getPreviousModuleOutput().turn_signal_info;
   }
 
-  if (is_large_deviation(spline_shift_path.path)) {
+  if (is_large_deviation(selected_spline_shift_path.path)) {
     return getPreviousModuleOutput().turn_signal_info;
   }
 
@@ -1110,14 +1125,15 @@ auto StaticObstacleAvoidanceModule::getTurnSignal(
   constexpr bool egos_lane_is_shifted = true;
 
   const auto [new_signal, is_ignore] = planner_data_->getBehaviorTurnSignalInfo(
-    linear_shift_path, target_shift_line, avoid_data_.current_lanelets, helper_->getEgoShift(),
-    is_driving_forward, egos_lane_is_shifted);
+    selected_linear_shift_path, target_shift_line, avoid_data_.current_lanelets,
+    helper_->getEgoShift(), is_driving_forward, egos_lane_is_shifted);
 
   update_ignore_signal(target_shift_line.id, is_ignore);
 
-  const auto current_seg_idx = planner_data_->findEgoSegmentIndex(spline_shift_path.path.points);
+  const auto current_seg_idx =
+    planner_data_->findEgoSegmentIndex(selected_spline_shift_path.path.points);
   return planner_data_->turn_signal_decider.overwrite_turn_signal(
-    spline_shift_path.path, getEgoPose(), current_seg_idx, original_signal, new_signal,
+    selected_spline_shift_path.path, getEgoPose(), current_seg_idx, original_signal, new_signal,
     planner_data_->parameters.ego_nearest_dist_threshold,
     planner_data_->parameters.ego_nearest_yaw_threshold);
 }
@@ -1299,7 +1315,9 @@ BehaviorModuleOutput StaticObstacleAvoidanceModule::planWaitingApproval()
   autoware_utils::ScopedTimeTrack st(__func__, *time_keeper_);
   BehaviorModuleOutput out = plan();
 
-  if (path_shifter_.getShiftLines().empty()) {
+  const bool is_no_shift_lines = !avoid_data_.yield_required ? path_shifter_.getShiftLines().empty()
+                                                             : avoid_data_.safe_shift_line.empty();
+  if (is_no_shift_lines) {
     out.turn_signal_info = getPreviousModuleOutput().turn_signal_info;
   }
 
