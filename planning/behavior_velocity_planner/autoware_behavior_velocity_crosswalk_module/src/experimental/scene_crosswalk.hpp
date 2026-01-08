55b7dbf90d90c79b6aa4d101aa79f339f6036a8f
mraditya01 <mraditya01@yahoo.com>
2026-01-08 17:12:08 +0900

    reverse guard

--- planning/behavior_velocity_planner/autoware_behavior_velocity_crosswalk_module/src/experimental/scene_crosswalk.hpp
+++ planning/behavior_velocity_planner/autoware_behavior_velocity_crosswalk_module/src/experimental/scene_crosswalk.hpp
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#ifndef SCENE_CROSSWALK_HPP_
-#define SCENE_CROSSWALK_HPP_
+#ifndef EXPERIMENTAL__SCENE_CROSSWALK_HPP_
+#define EXPERIMENTAL__SCENE_CROSSWALK_HPP_
 
 #include "autoware/behavior_velocity_crosswalk_module/util.hpp"
 
