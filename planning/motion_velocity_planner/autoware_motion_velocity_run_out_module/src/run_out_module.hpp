785a445a7d29a7ebb1488fd98484fb8b0cf225d7
Maxime CLEMENT <maxime.clement@tier4.jp>
2026-01-12 20:20:38 +0900

    remove previous debug publisher of the run_out module
    Signed-off-by: Maxime CLEMENT <maxime.clement@tier4.jp>

--- planning/motion_velocity_planner/autoware_motion_velocity_run_out_module/src/run_out_module.hpp
+++ planning/motion_velocity_planner/autoware_motion_velocity_run_out_module/src/run_out_module.hpp
@@ -74,7 +74,6 @@ private:
   rclcpp::Clock::SharedPtr clock_{nullptr};
   // TODO(Maxime): move to the module interface
   rclcpp::Publisher<universe_utils::ProcessingTimeDetail>::SharedPtr timekeeper_publisher_;
-  rclcpp::Publisher<autoware_planning_msgs::msg::Trajectory>::SharedPtr debug_trajectory_publisher_;
   std::shared_ptr<autoware::universe_utils::TimeKeeper> time_keeper_;
   std::optional<diagnostic_updater::Updater> diagnostic_updater_ = std::nullopt;
   std::unique_ptr<autoware::objects_of_interest_marker_interface::ObjectsOfInterestMarkerInterface>
