a521a4627ef5ad314b3c9b761f966b3971ab1929
TakahikoHasegawa97 <takahiko.hasegawa@tier4.jp>
2026-01-23 18:54:55 +0900

    feat(crosswalk): force ego to stop for pedestrians on crosswalk regardless of any other conditions
    Signed-off-by: TakahikoHasegawa97 <takahiko.hasegawa@tier4.jp>

--- planning/behavior_velocity_planner/autoware_behavior_velocity_crosswalk_module/src/manager.cpp
+++ planning/behavior_velocity_planner/autoware_behavior_velocity_crosswalk_module/src/manager.cpp
@@ -110,6 +110,8 @@ CrosswalkModuleManager::CrosswalkModuleManager(rclcpp::Node & node)
     get_or_declare_parameter<double>(node, ns + ".pass_judge.ego_min_assumed_speed");
   cp.consider_obj_on_crosswalk_on_red_light =
     get_or_declare_parameter<bool>(node, ns + ".pass_judge.consider_obj_on_crosswalk_on_red_light");
+  cp.stop_for_pedestrian_on_crosswalk =
+    get_or_declare_parameter<bool>(node, ns + ".pass_judge.stop_for_pedestrian_on_crosswalk");
   cp.enable_no_stop_decision = get_or_declare_parameter<bool>(
     node, ns + ".pass_judge.no_stop_decision.enable_no_stop_decision");
   cp.min_acc_for_no_stop_decision =
