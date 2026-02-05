58259fcb9ff6a57efca1a9a31c7a017f51257c22
Takahisa.Ishikawa <takahisa.ishikawa@tier4.jp>
2026-02-05 11:09:08 +0900

    test(blockage_diag): optimize parameters and remove unnecessary threading in integration tests
    Signed-off-by: Takahisa.Ishikawa <takahisa.ishikawa@tier4.jp>

--- sensing/autoware_pointcloud_preprocessor/test/blockage_diag/test_blockage_diag_node.cpp
+++ sensing/autoware_pointcloud_preprocessor/test/blockage_diag/test_blockage_diag_node.cpp
@@ -26,7 +26,6 @@
 #include <cmath>
 #include <memory>
 #include <string>
-#include <thread>
 
 class BlockageDiagIntegrationTest : public ::testing::Test
 {
@@ -44,12 +43,12 @@ protected:
     node_options.append_parameter_override("enable_dust_diag", true);
     node_options.append_parameter_override("dust_ratio_threshold", 0.3);
     node_options.append_parameter_override("dust_count_threshold", 2);
-    node_options.append_parameter_override("dust_kernel_size", 3);
+    node_options.append_parameter_override("dust_kernel_size", 1);
     node_options.append_parameter_override("dust_buffering_frames", 5);
     node_options.append_parameter_override("dust_buffering_interval", 2);
     node_options.append_parameter_override("blockage_ratio_threshold", 0.5);
     node_options.append_parameter_override("blockage_count_threshold", 2);
-    node_options.append_parameter_override("blockage_kernel", 3);
+    node_options.append_parameter_override("blockage_kernel", 1);
     node_options.append_parameter_override("blockage_buffering_frames", 5);
     node_options.append_parameter_override("blockage_buffering_interval", 2);
     node_options.append_parameter_override("publish_debug_image", true);
@@ -68,12 +67,6 @@ protected:
     executor_->add_node(blockage_diag_node_);
     executor_->add_node(test_node_);
 
-    // Start executor in a separate thread
-    executor_thread_ = std::thread([this]() { executor_->spin(); });
-
-    // Wait for node setup
-    std::this_thread::sleep_for(std::chrono::milliseconds(200));
-
     // Create publisher for input topic (blockage_diag subscribes to "input")
     input_pub_ =
       test_node_->create_publisher<sensor_msgs::msg::PointCloud2>("input", rclcpp::SensorDataQoS());
@@ -86,19 +79,9 @@ protected:
         diagnostics_msg_ = msg;
         diagnostics_received_ = true;
       });
-
-    // Wait for publisher/subscriber setup
-    std::this_thread::sleep_for(std::chrono::milliseconds(200));
   }
 
-  void TearDown() override
-  {
-    executor_->cancel();
-    if (executor_thread_.joinable()) {
-      executor_thread_.join();
-    }
-    rclcpp::shutdown();
-  }
+  void TearDown() override { rclcpp::shutdown(); }
 
   sensor_msgs::msg::PointCloud2 create_valid_pointcloud()
   {
@@ -161,7 +144,7 @@ protected:
       if (std::chrono::steady_clock::now() - start > timeout) {
         return false;
       }
-      std::this_thread::sleep_for(std::chrono::milliseconds(10));
+      executor_->spin_some();
     }
 
     return true;
@@ -170,7 +153,6 @@ protected:
   std::shared_ptr<autoware::pointcloud_preprocessor::BlockageDiagComponent> blockage_diag_node_;
   std::shared_ptr<rclcpp::Node> test_node_;
   std::shared_ptr<rclcpp::executors::SingleThreadedExecutor> executor_;
-  std::thread executor_thread_;
   rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr input_pub_;
   rclcpp::Subscription<diagnostic_msgs::msg::DiagnosticArray>::SharedPtr diagnostics_sub_;
   diagnostic_msgs::msg::DiagnosticArray::SharedPtr diagnostics_msg_;
