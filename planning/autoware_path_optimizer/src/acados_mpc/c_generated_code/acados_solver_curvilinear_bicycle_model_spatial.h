cc31944c0ee26fcb694732e0fede314803b29e58
Arjun Jagdish Ram <arjun.ram@tier4.jp>
2026-03-23 09:42:18 +0900

    fix for copyright
    Signed-off-by: Arjun Jagdish Ram <arjun.ram@tier4.jp>

--- planning/autoware_path_optimizer/src/acados_mpc/c_generated_code/acados_solver_curvilinear_bicycle_model_spatial.h
+++ planning/autoware_path_optimizer/src/acados_mpc/c_generated_code/acados_solver_curvilinear_bicycle_model_spatial.h
@@ -1,21 +1,19 @@
-/*
- * Copyright 2026 TIER IV, Inc.
- *
- * Stub header for static analysis (clang-tidy) and IDE support.
- * The real header is generated at build time into the build directory.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2025 TIER IV, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// Stub header for static analysis (clang-tidy) and IDE support.
+// The real header is generated at build time into the build directory.
 
 #ifndef ACADOS_MPC__C_GENERATED_CODE__ACADOS_SOLVER_CURVILINEAR_BICYCLE_MODEL_SPATIAL_H_
 #define ACADOS_MPC__C_GENERATED_CODE__ACADOS_SOLVER_CURVILINEAR_BICYCLE_MODEL_SPATIAL_H_
