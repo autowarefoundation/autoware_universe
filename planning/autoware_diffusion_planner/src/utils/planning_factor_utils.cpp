e1ff2332b89c511f14335b9715a5bdbf3a47422e
Kotakku <kotakkucu@gmail.com>
2026-03-31 13:48:04 +0900

    split functions
    Signed-off-by: Kotakku <kotakkucu@gmail.com>

--- planning/autoware_diffusion_planner/src/utils/planning_factor_utils.cpp
+++ planning/autoware_diffusion_planner/src/utils/planning_factor_utils.cpp
@@ -23,23 +23,14 @@ namespace autoware::diffusion_planner
 
 using autoware_planning_msgs::msg::TrajectoryPoint;
 
-PlanningFactorDetectionResult detect_planning_factors(
+std::optional<DetectedStopFactor> detect_stop_factor(
   const std::vector<TrajectoryPoint> & points, const PlanningFactorDetectionConfig & config)
 {
-  PlanningFactorDetectionResult result;
-
-  if (points.empty()) {
-    return result;
-  }
-
-  std::optional<size_t> slowdown_start_idx;
-  std::optional<size_t> slowdown_end_idx;
   std::optional<size_t> stop_idx;
   rclcpp::Duration stop_start_time(0, 0);
   bool is_valid_stop = true;
 
   for (size_t i = 0; i < points.size(); ++i) {
-    // for stop
     if (!stop_idx && points[i].longitudinal_velocity_mps <= config.stop_velocity_threshold) {
       stop_idx = i;
       stop_start_time = rclcpp::Duration(points[i].time_from_start);
@@ -52,8 +43,25 @@ PlanningFactorDetectionResult detect_planning_factors(
           is_valid_stop = false;
       }
     }
+  }
 
-    // for slowdown
+  if (!stop_idx || !is_valid_stop) {
+    return std::nullopt;
+  }
+
+  DetectedStopFactor stop;
+  stop.ego_pose = points[0].pose;
+  stop.stop_pose = points[*stop_idx].pose;
+  return stop;
+}
+
+std::optional<DetectedSlowdownFactor> detect_slowdown_factor(
+  const std::vector<TrajectoryPoint> & points, const PlanningFactorDetectionConfig & config)
+{
+  std::optional<size_t> slowdown_start_idx;
+  std::optional<size_t> slowdown_end_idx;
+
+  for (size_t i = 0; i < points.size(); ++i) {
     if (points[i].acceleration_mps2 < config.slowdown_accel_threshold) {
       if (!slowdown_start_idx) slowdown_start_idx = i;
     } else if (slowdown_start_idx && !slowdown_end_idx) {
@@ -61,27 +69,30 @@ PlanningFactorDetectionResult detect_planning_factors(
     }
   }
 
-  // stop
-  if (stop_idx && is_valid_stop) {
-    DetectedStopFactor stop;
-    stop.ego_pose = points[0].pose;
-    stop.stop_pose = points[*stop_idx].pose;
-    result.stop = stop;
+  if (!slowdown_start_idx || !slowdown_end_idx) {
+    return std::nullopt;
   }
 
-  // slowdown
-  if (slowdown_start_idx && slowdown_end_idx) {
-    DetectedSlowdownFactor slowdown;
-    slowdown.ego_pose = points[0].pose;
-    slowdown.start_pose = points[*slowdown_start_idx].pose;
-    slowdown.end_pose = points[*slowdown_end_idx].pose;
-    slowdown.start_velocity =
-      static_cast<double>(points[*slowdown_start_idx].longitudinal_velocity_mps);
-    slowdown.end_velocity =
-      static_cast<double>(points[*slowdown_end_idx].longitudinal_velocity_mps);
-    result.slowdown = slowdown;
+  DetectedSlowdownFactor slowdown;
+  slowdown.ego_pose = points[0].pose;
+  slowdown.start_pose = points[*slowdown_start_idx].pose;
+  slowdown.end_pose = points[*slowdown_end_idx].pose;
+  slowdown.start_velocity = points[*slowdown_start_idx].longitudinal_velocity_mps;
+  slowdown.end_velocity = points[*slowdown_end_idx].longitudinal_velocity_mps;
+  return slowdown;
+}
+
+PlanningFactorDetectionResult detect_planning_factors(
+  const std::vector<TrajectoryPoint> & points, const PlanningFactorDetectionConfig & config)
+{
+  PlanningFactorDetectionResult result;
+
+  if (points.empty()) {
+    return result;
   }
 
+  result.stop = detect_stop_factor(points, config);
+  result.slowdown = detect_slowdown_factor(points, config);
   return result;
 }
 
