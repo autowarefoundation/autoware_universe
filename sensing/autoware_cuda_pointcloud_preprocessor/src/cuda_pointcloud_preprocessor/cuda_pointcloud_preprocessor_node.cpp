9f4c36590d566663c5a5a224990e6e0b23b43a96
Koichi Imai <koichi.imai.2@tier4.jp>
2026-06-02 11:09:08 +0900

    delete suppress comments of autoware_agnocast_wrapper macro
    Signed-off-by: Koichi Imai <koichi.imai.2@tier4.jp>

--- sensing/autoware_cuda_pointcloud_preprocessor/src/cuda_pointcloud_preprocessor/cuda_pointcloud_preprocessor_node.cpp
+++ sensing/autoware_cuda_pointcloud_preprocessor/src/cuda_pointcloud_preprocessor/cuda_pointcloud_preprocessor_node.cpp
@@ -127,7 +127,6 @@ CudaPointcloudPreprocessorNode::CudaPointcloudPreprocessorNode(
 
   sub_options.callback_group = pointcloud_callback_group_;
 
-  // cppcheck-suppress unknownMacro
   pointcloud_sub_ = AUTOWARE_CREATE_SUBSCRIPTION(
     sensor_msgs::msg::PointCloud2, "~/input/pointcloud", rclcpp::SensorDataQoS{}.keep_last(1),
     std::bind(&CudaPointcloudPreprocessorNode::pointcloudCallback, this, std::placeholders::_1),
@@ -263,7 +262,6 @@ void CudaPointcloudPreprocessorNode::imuCallback(const sensor_msgs::msg::Imu & i
 }
 
 void CudaPointcloudPreprocessorNode::pointcloudCallback(
-  // cppcheck-suppress unknownMacro
   AUTOWARE_MESSAGE_UNIQUE_PTR(sensor_msgs::msg::PointCloud2) input_pointcloud_msg_ptr)
 {
   const auto & input_pointcloud_msg = *input_pointcloud_msg_ptr;
--- sensing/autoware_pointcloud_preprocessor/include/autoware/pointcloud_preprocessor/outlier_filter/polar_voxel_outlier_filter_node.hpp
+++ sensing/autoware_pointcloud_preprocessor/include/autoware/pointcloud_preprocessor/outlier_filter/polar_voxel_outlier_filter_node.hpp
@@ -258,7 +258,6 @@ protected:
   OnSetParametersCallbackHandle::SharedPtr set_param_res_;
 
   // TODO(Koichi98): Remove once Filter base class supports agnocast_wrapper.
-  // cppcheck-suppress unknownMacro
   AUTOWARE_SUBSCRIPTION_PTR(sensor_msgs::msg::PointCloud2) agnocast_sub_input_;
 
   // Diagnostic helper methods
