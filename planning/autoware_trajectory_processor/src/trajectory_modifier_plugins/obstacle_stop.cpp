be219b9b746d68bbf6adb01e39b2e52acb1a1b2f
kobayu858 <yutaro.kobayashi.2@tier4.jp>
2026-08-20 15:29:26 +0900

    feat: apply agnocast_wrapper::Node to trajectory_processor
    Signed-off-by: kobayu858 <yutaro.kobayashi.2@tier4.jp>

--- planning/autoware_trajectory_processor/src/trajectory_modifier_plugins/obstacle_stop.cpp
+++ planning/autoware_trajectory_processor/src/trajectory_modifier_plugins/obstacle_stop.cpp
@@ -43,18 +43,13 @@ using utils::obstacle_stop::PointCloud2;
 
 void ObstacleStop::on_initialize(const TrajectoryProcessorParams & params)
 {
-  const auto node_ptr = get_node_ptr();
-  planning_factor_interface_ =
-    std::make_unique<autoware::planning_factor_interface::PlanningFactorInterface>(
-      node_ptr, "modifier_obstacle_stop");
+  init_planning_factor_interface("modifier_obstacle_stop");
 
-  pub_clustered_pointcloud_ =
-    node_ptr->create_publisher<PointCloud2>("~/obstacle_stop/debug/cluster_points", 1);
-  pub_filtered_pointcloud_ =
-    node_ptr->create_publisher<PointCloud2>("~/obstacle_stop/debug/filtered_points", 1);
-  debug_viz_pub_ = node_ptr->create_publisher<visualization_msgs::msg::MarkerArray>(
-    "~/obstacle_stop/debug/marker", 1);
-  pub_debug_text_ = node_ptr->create_publisher<StringStamped>("~/obstacle_stop/debug/text", 1);
+  pub_clustered_pointcloud_ = make_publisher<PointCloud2>("~/obstacle_stop/debug/cluster_points");
+  pub_filtered_pointcloud_ = make_publisher<PointCloud2>("~/obstacle_stop/debug/filtered_points");
+  debug_viz_pub_ =
+    make_publisher<visualization_msgs::msg::MarkerArray>("~/obstacle_stop/debug/marker", 1);
+  pub_debug_text_ = make_publisher<StringStamped>("~/obstacle_stop/debug/text");
 
   params_ = params.obstacle_stop;
   stopping_params_ = params.stopping_constraints;
@@ -222,11 +217,10 @@ bool ObstacleStop::set_stop_point(
     input.current_acceleration->accel.accel.linear.x, stopping_params_.maximum_deceleration,
     stopping_params_.jerk_limit);
 
-  if (
-    utils::stop_point_exists(
-      traj_points, target_stop_point_arc_length, params_.duplicate_check_threshold)) {
+  if (utils::stop_point_exists(
+        traj_points, target_stop_point_arc_length, params_.duplicate_check_threshold)) {
     RCLCPP_WARN_THROTTLE(
-      get_node_ptr()->get_logger(), *get_clock(), 1000,
+      get_logger(), *get_clock(), 1000,
       "[TM ObstacleStop] Preceding (or duplicate) stop point exists, skip inserting stop point");
     return false;
   }
@@ -249,8 +243,8 @@ bool ObstacleStop::set_stop_point(
     safety_factors_);
 
   RCLCPP_WARN_THROTTLE(
-    get_node_ptr()->get_logger(), *get_clock(), 1000,
-    "[TM ObstacleStop] Inserted stop point at arc length %f m", target_stop_point_arc_length);
+    get_logger(), *get_clock(), 1000, "[TM ObstacleStop] Inserted stop point at arc length %f m",
+    target_stop_point_arc_length);
   return true;
 }
 
@@ -273,7 +267,7 @@ void ObstacleStop::check_obstacles(
 
   if (collision_point_objects) {
     RCLCPP_WARN_THROTTLE(
-      get_node_ptr()->get_logger(), *get_clock(), 1000,
+      get_logger(), *get_clock(), 1000,
       "[TM ObstacleStop] Detected collision with object at arc length %f m",
       collision_point_objects->arc_length);
     if (debug_data_.colliding_object) {
@@ -287,7 +281,7 @@ void ObstacleStop::check_obstacles(
 
   if (collision_point_pcd) {
     RCLCPP_WARN_THROTTLE(
-      get_node_ptr()->get_logger(), *get_clock(), 1000,
+      get_logger(), *get_clock(), 1000,
       "[TM ObstacleStop] Detected collision with pointcloud at arc length %f m",
       collision_point_pcd->arc_length);
     auto safety_factor = get_safety_factor(collision_point_pcd->point, SafetyFactor::POINTCLOUD);
@@ -383,7 +377,7 @@ std::optional<CollisionPoint> ObstacleStop::check_pointcloud(
       transform_stamped = context_->tf_buffer.lookupTransform(
         "map", input.obstacle_pointcloud->header.frame_id, tf2::TimePointZero);
     } catch (tf2::TransformException & e) {
-      RCLCPP_WARN(get_node_ptr()->get_logger(), "no transform found for pointcloud: %s", e.what());
+      RCLCPP_WARN(get_logger(), "no transform found for pointcloud: %s", e.what());
       return std::nullopt;
     }
 
@@ -454,13 +448,13 @@ void ObstacleStop::publish_debug_string(bool is_safe) const
   StringStamped string_stamp;
   string_stamp.stamp = get_clock()->now();
   string_stamp.data = ss.str();
-  pub_debug_text_->publish(string_stamp);
+  pub_debug_text_(string_stamp);
 }
 
 void ObstacleStop::publish_debug_data(const std::string & ns) const
 {
-  if (debug_data_.filtered_points) pub_filtered_pointcloud_->publish(*debug_data_.filtered_points);
-  if (debug_data_.cluster_points) pub_clustered_pointcloud_->publish(*debug_data_.cluster_points);
+  if (debug_data_.filtered_points) pub_filtered_pointcloud_(*debug_data_.filtered_points);
+  if (debug_data_.cluster_points) pub_clustered_pointcloud_(*debug_data_.cluster_points);
 
   MarkerArray marker_array;
   const auto ego_z = debug_data_.ego_z;
@@ -525,7 +519,7 @@ void ObstacleStop::publish_debug_data(const std::string & ns) const
     id++;
   }
 
-  debug_viz_pub_->publish(marker_array);
+  debug_viz_pub_(marker_array);
 }
 
 }  // namespace autoware::trajectory_processor::plugin
