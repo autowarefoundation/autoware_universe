4480950c8fdf3672629d3a89657a77684c39cdbd
kobayu858 <yutaro.kobayashi.2@tier4.jp>
2026-08-24 21:48:20 +0900

    fix(mrm_handler): migrate to the current agnocast_wrapper polling API
    Signed-off-by: kobayu858 <yutaro.kobayashi.2@tier4.jp>

--- system/autoware_mrm_handler/include/autoware/mrm_handler/mrm_handler_core.hpp
+++ system/autoware_mrm_handler/include/autoware/mrm_handler/mrm_handler_core.hpp
@@ -24,6 +24,7 @@
 // Autoware
 #include <autoware/agnocast_wrapper/autoware_agnocast_wrapper.hpp>
 #include <autoware/agnocast_wrapper/node.hpp>
+#include <autoware/agnocast_wrapper/polling_subscriber.hpp>
 
 #include <autoware_adapi_v1_msgs/msg/mrm_state.hpp>
 #include <autoware_adapi_v1_msgs/msg/operation_mode_state.hpp>
@@ -83,33 +84,33 @@ private:
   AUTOWARE_SUBSCRIPTION_PTR(tier4_system_msgs::msg::OperationModeAvailability)
   sub_operation_mode_availability_;
   // Subscribers without callback
-  AUTOWARE_POLLING_SUBSCRIBER_PTR(nav_msgs::msg::Odometry)
-  sub_odom_ {
-    create_polling_subscriber<nav_msgs::msg::Odometry>("~/input/odometry")
-  };
-  AUTOWARE_POLLING_SUBSCRIBER_PTR(autoware_vehicle_msgs::msg::ControlModeReport)
-  sub_control_mode_ {
-    create_polling_subscriber<autoware_vehicle_msgs::msg::ControlModeReport>("~/input/control_mode")
-  };
-  AUTOWARE_POLLING_SUBSCRIBER_PTR(tier4_system_msgs::msg::MrmBehaviorStatus)
-  sub_mrm_pull_over_status_{create_polling_subscriber<tier4_system_msgs::msg::MrmBehaviorStatus>(
-    "~/input/mrm/pull_over/status")};
-  AUTOWARE_POLLING_SUBSCRIBER_PTR(tier4_system_msgs::msg::MrmBehaviorStatus)
-  sub_mrm_comfortable_stop_status_{
-    create_polling_subscriber<tier4_system_msgs::msg::MrmBehaviorStatus>(
-      "~/input/mrm/comfortable_stop/status")};
-  AUTOWARE_POLLING_SUBSCRIBER_PTR(tier4_system_msgs::msg::MrmBehaviorStatus)
-  sub_mrm_emergency_stop_status_{
-    create_polling_subscriber<tier4_system_msgs::msg::MrmBehaviorStatus>(
-      "~/input/mrm/emergency_stop/status")};
-  AUTOWARE_POLLING_SUBSCRIBER_PTR(autoware_adapi_v1_msgs::msg::OperationModeState)
-  sub_operation_mode_state_{
-    create_polling_subscriber<autoware_adapi_v1_msgs::msg::OperationModeState>(
-      "~/input/api/operation_mode/state")};
-  AUTOWARE_POLLING_SUBSCRIBER_PTR(autoware_vehicle_msgs::msg::GearCommand)
-  sub_gear_cmd_ {
-    create_polling_subscriber<autoware_vehicle_msgs::msg::GearCommand>("~/input/gear")
-  };
+  autoware::agnocast_wrapper::polling::PollingSubscriber<nav_msgs::msg::Odometry>::SharedPtr
+    sub_odom_ = autoware::agnocast_wrapper::polling::create_polling_subscriber<
+      nav_msgs::msg::Odometry>(this, "~/input/odometry");
+  autoware::agnocast_wrapper::polling::PollingSubscriber<
+    autoware_vehicle_msgs::msg::ControlModeReport>::SharedPtr sub_control_mode_ =
+    autoware::agnocast_wrapper::polling::create_polling_subscriber<
+      autoware_vehicle_msgs::msg::ControlModeReport>(this, "~/input/control_mode");
+  autoware::agnocast_wrapper::polling::PollingSubscriber<
+    tier4_system_msgs::msg::MrmBehaviorStatus>::SharedPtr sub_mrm_pull_over_status_ =
+    autoware::agnocast_wrapper::polling::create_polling_subscriber<
+      tier4_system_msgs::msg::MrmBehaviorStatus>(this, "~/input/mrm/pull_over/status");
+  autoware::agnocast_wrapper::polling::PollingSubscriber<
+    tier4_system_msgs::msg::MrmBehaviorStatus>::SharedPtr sub_mrm_comfortable_stop_status_ =
+    autoware::agnocast_wrapper::polling::create_polling_subscriber<
+      tier4_system_msgs::msg::MrmBehaviorStatus>(this, "~/input/mrm/comfortable_stop/status");
+  autoware::agnocast_wrapper::polling::PollingSubscriber<
+    tier4_system_msgs::msg::MrmBehaviorStatus>::SharedPtr sub_mrm_emergency_stop_status_ =
+    autoware::agnocast_wrapper::polling::create_polling_subscriber<
+      tier4_system_msgs::msg::MrmBehaviorStatus>(this, "~/input/mrm/emergency_stop/status");
+  autoware::agnocast_wrapper::polling::PollingSubscriber<
+    autoware_adapi_v1_msgs::msg::OperationModeState>::SharedPtr sub_operation_mode_state_ =
+    autoware::agnocast_wrapper::polling::create_polling_subscriber<
+      autoware_adapi_v1_msgs::msg::OperationModeState>(this, "~/input/api/operation_mode/state");
+  autoware::agnocast_wrapper::polling::PollingSubscriber<
+    autoware_vehicle_msgs::msg::GearCommand>::SharedPtr sub_gear_cmd_ =
+    autoware::agnocast_wrapper::polling::create_polling_subscriber<
+      autoware_vehicle_msgs::msg::GearCommand>(this, "~/input/gear");
 
   AUTOWARE_MESSAGE_CONST_SHARED_PTR(tier4_system_msgs::msg::OperationModeAvailability)
   operation_mode_availability_;
