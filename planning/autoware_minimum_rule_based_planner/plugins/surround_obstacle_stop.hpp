Owen-Liuyuxuan/autoware.universe

filter surround obstacle pointclouds by type and range
- Parse PointXYZCPE clouds in surround_obstacle_stop and keep only configured semantic labels before proximity checks
- Crop to the ego footprint expanded by front/side/back thresholds and hysteresis, then convert to PointXYZ for ProximityChecker
- Add target_objects.pointcloud params (default hazard/structure/vegetation) to config, parameter structs, and schemas in both packages
- Allow unknown labels in trajectory_modifier surround integration tests so XYZ-only fixtures still exercise the PCD path

Signed-off-by: Mohammad <alqudah.mohammad@tier4.jp>

planning/autoware_minimum_rule_based_planner/plugins/surround_obstacle_stop.hpp
@@ -18,6 +18,7 @@
 #include "autoware/obstacle_proximity_checker/obstacle_proximity_checker.hpp"
 #include "plugin_interface.hpp"
 
+#include <autoware/trajectory_modifier/trajectory_modifier_utils/obstacle_stop_utils.hpp>
 #include <rclcpp/rclcpp.hpp>
 
 #include <autoware_internal_debug_msgs/msg/string_stamped.hpp>
@@ -33,6 +34,7 @@ namespace autoware::minimum_rule_based_planner::plugin
 using autoware_internal_debug_msgs::msg::StringStamped;
 using autoware_planning_msgs::msg::TrajectoryPoint;
 using TrajectoryPoints = std::vector<TrajectoryPoint>;
+using autoware::trajectory_modifier::utils::obstacle_stop::PointCloud;
 
 class SurroundObstacleStop : public PluginInterface
 {
@@ -52,6 +54,7 @@ private:
   MinimumRuleBasedPlannerParams::SurroundObstacleStop params_;
 
   std::unique_ptr<obstacle_proximity_checker::ProximityChecker> proximity_checker_;
+  std::unique_ptr<trajectory_modifier::utils::obstacle_stop::PointCloudFilter> pointcloud_filter_;
 
   std::optional<obstacle_proximity_checker::CheckResult> proximity_check_result_;
 

autowarefoundation/autoware_universe

planning/autoware_minimum_rule_based_planner/plugins/surround_obstacle_stop.hpp
@@ -18,6 +18,7 @@
 #include "autoware/obstacle_proximity_checker/obstacle_proximity_checker.hpp"
 #include "plugin_interface.hpp"
 
+#include <autoware/trajectory_processor/trajectory_modifier_utils/obstacle_stop_utils.hpp>
 #include <rclcpp/rclcpp.hpp>
 
 #include <autoware_internal_debug_msgs/msg/string_stamped.hpp>
@@ -33,6 +34,7 @@ namespace autoware::minimum_rule_based_planner::plugin
 using autoware_internal_debug_msgs::msg::StringStamped;
 using autoware_planning_msgs::msg::TrajectoryPoint;
 using TrajectoryPoints = std::vector<TrajectoryPoint>;
+using autoware::trajectory_processor::utils::obstacle_stop::PointCloud;
 
 class SurroundObstacleStop : public PluginInterface
 {
@@ -52,6 +54,7 @@ private:
   MinimumRuleBasedPlannerParams::SurroundObstacleStop params_;
 
   std::unique_ptr<obstacle_proximity_checker::ProximityChecker> proximity_checker_;
+  std::unique_ptr<trajectory_processor::utils::obstacle_stop::PointCloudFilter> pointcloud_filter_;
 
   std::optional<obstacle_proximity_checker::CheckResult> proximity_check_result_;
 