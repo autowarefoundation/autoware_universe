f7034d7c287c4bfedb3632ca327787f5825210a7
Koichi Imai <koichi.imai.2@tier4.jp>
2026-09-16 07:53:26 +0900

    feat(autoware_topic_relay_controller): add throttle mode
    Signed-off-by: Koichi Imai <koichi.imai.2@tier4.jp>

--- system/autoware_topic_relay_controller/src/topic_relay_controller_node.hpp
+++ system/autoware_topic_relay_controller/src/topic_relay_controller_node.hpp
@@ -42,6 +42,8 @@ struct NodeParam
   std::string srv_name;
   bool enable_keep_publishing;
   int update_rate;
+  bool enable_throttle;
+  double msgs_per_sec;
 };
 
 class TopicRelayController : public autoware::agnocast_wrapper::Node
@@ -50,6 +52,12 @@ public:
   explicit TopicRelayController(const rclcpp::NodeOptions & options);
 
 private:
+  void on_topic(std::shared_ptr<const rclcpp::SerializedMessage> msg);
+
+  /// Rate gate for the throttle mode: true once the throttle period has elapsed since the last
+  /// relayed message, and unconditionally true when enable_throttle is false.
+  bool is_throttle_period_elapsed();
+
   // Parameter
   NodeParam node_param_;
 
@@ -69,6 +77,8 @@ private:
 
   // State
   bool is_relaying_;
+  rclcpp::Duration throttle_period_;
+  rclcpp::Time last_relayed_time_;
   tf2_msgs::msg::TFMessage::ConstSharedPtr last_tf_topic_;
   std::shared_ptr<const rclcpp::SerializedMessage> last_topic_;
 };
